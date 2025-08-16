be4a8aaa404e72d5a3d15793fe029805629548f3
ShiomiKazuma <kazu_shio_1217@icloud.com>
2025-08-16 20:06:06 +0900

    [fix]ペンギンの処理を修正

--- Assets/Scripts/InGame/Player/Tanihira/PenguinFriend.cs
+++ Assets/Scripts/InGame/Player/Tanihira/PenguinFriend.cs
@@ -11,6 +11,7 @@ public class PenguinFriend : FriendBase
         //ペンギンのステートを設定
         _friendStateMappings[FriendState.Idle] = null;
         _friendStateMappings[FriendState.Move] = new FriendMoveState();
+        _friendStateMappings[FriendState.Chase] = new FriendChaseState();
         _friendStateMappings[FriendState.Attack] = new FriendAttackState();
     }
 }
