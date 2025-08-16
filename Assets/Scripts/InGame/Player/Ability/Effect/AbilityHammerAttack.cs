14ef448ef6a4ff82f65e56760399351d39d1191b
TsuyoshiUsugi <qesulive@gmail.com>
2025-08-16 13:39:59 +0900

    ひとまず攻撃できるように

--- Assets/Scripts/InGame/Player/Ability/Effect/AbilityHammerAttack.cs
+++ Assets/Scripts/InGame/Player/Ability/Effect/AbilityHammerAttack.cs
@@ -13,8 +13,8 @@ namespace InGame.Player.Ability.Effect
         [SerializeField] private AnimationClip _normalAttackAnimationClip;
         [SerializeField] private LayerMask _hitMask;
         private int _attackDamage = int.MaxValue;
-        private MeleeHitboxExecutor _executor;
-
+        private HitChecker _hitChecker;
+        
         protected override void OnStart()
         {
             var ownerAnimator = Parameter.Owner.GetComponent<AnimationClipPlayer>();
@@ -22,44 +22,12 @@ namespace InGame.Player.Ability.Effect
             {
                 ownerAnimator.PlayClip(_normalAttackAnimationClip, 1, 0, true);
             }
-            var resolver = Parameter.Owner.GetComponentInChildren<HitPointResolver>();
-            var points = resolver?.GetPoints();
-            var start = resolver?.GetStartFrame();
-            var end = resolver?.GetEndFrame();
-            var radius = resolver?.GetRadius() ?? 0.1f;
-            _executor = new MeleeHitboxExecutor(points, radius, _hitMask, start ?? 0, end ?? int.MaxValue)
-            {
-                OnHit = collider =>
-                {
-                    //自分に当たった場合は無視
-                    if (collider.GetComponentInParent<NetworkObject>() == Parameter.Owner) return;
-                    
-                    var damageable = collider.GetComponentInParent<IDamageable>();
-                    if (damageable != null)
-                    {
-                        var hitData = new HitData(
-                            HitActionType.Damage,
-                            _attackDamage,
-                            Parameter.Owner.InputAuthority,
-                            damageable.OwnerPlayerRef);
-                        damageable.TakeHit(ref hitData);
-                    }
-                    
-                    var interactable = collider.GetComponentInParent<DisableInteractEffect>();
-                    if (interactable != null)
-                    {
-                        interactable.OnHitHammerAttack();
-                    }
-                    
-                    // ヒットエフェクトを再生
-                }
-            };
+
         }
 
         protected override void OnUpdate(float deltaTime)
         {
-            _executor.Tick(deltaTime);
-            if (_executor.IsFinished)
+            if (_hitChecker.IsFinished)
             {
                 _phase = AbilityPhase.Ending;
             }
