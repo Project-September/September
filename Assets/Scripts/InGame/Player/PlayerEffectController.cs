c2b329d9b2102e68ffbb840f6012469780340f27
TsuyoshiUsugi <qesulive@gmail.com>
2025-08-25 13:20:58 +0900

    殴りエフェクト修正

--- Assets/Scripts/InGame/Player/PlayerEffectController.cs
+++ Assets/Scripts/InGame/Player/PlayerEffectController.cs
@@ -1,29 +1,46 @@
+using System;
 using September.Common;
 using September.InGame.Effect;
+using UniRx;
 using UnityEngine;
 
 public class PlayerEffectController : MonoBehaviour
 {
-    [SerializeField] private Transform _punchEffectTransform;
+    [SerializeField] private ParticleSystem _punchEffect;
     [SerializeField] private Vector3 _punchEffectRotationOffset;
     [SerializeField] private Vector3 _punchEffectPositionOffset;
     [SerializeField] private Vector3 _stunEffectPositionOffset;
     private string _generateStunEffectId;
+    private Quaternion _basePunchRotation;
+    private IDisposable _followSub;
+
+    private void Start()
+    {
+        _basePunchRotation = Quaternion.Inverse(transform.rotation) * _punchEffect.transform.rotation;
+    }
 
     public void PlayPunchEffect()
     {
         var effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
-        if (effectSpawner && _punchEffectTransform)
+        if (effectSpawner && _punchEffect)
         {
-            effectSpawner.RequestPlayOneShotEffect(EffectType.PunchImpact,
-                _punchEffectTransform.position + _punchEffectPositionOffset, transform.rotation, _punchEffectTransform);
+            _followSub?.Dispose();
+            _punchEffect.Play();
+            _followSub = Observable.EveryLateUpdate()
+                .TakeWhile(_ => _punchEffect && _punchEffect.IsAlive(true) && _punchEffect.isPlaying)
+                .Subscribe(_ =>
+                {
+                    var baseRot = Quaternion.LookRotation(transform.forward, Vector3.up);
+                    _punchEffect.transform.rotation = baseRot;
+                })
+                .AddTo(this);
         }
     }
 
     public void PlayStunEffect()
     {
         var effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
-        if (effectSpawner && _punchEffectTransform)
+        if (effectSpawner)
         {
             _generateStunEffectId = System.Guid.NewGuid().ToString();
             effectSpawner.RequestPlayLoopEffect(_generateStunEffectId, EffectType.StunNormal,
