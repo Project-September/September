3e3de9b5302e409e03d8cee31c47c546663144df
TsuyoshiUsugi <qesulive@gmail.com>
2025-08-25 22:49:39 +0900

    パンチエフェクトの処理追加

--- Assets/Scripts/InGame/Player/PlayerEffectController.cs
+++ Assets/Scripts/InGame/Player/PlayerEffectController.cs
@@ -1,10 +1,11 @@
 using System;
+using Fusion;
 using September.Common;
 using September.InGame.Effect;
 using UniRx;
 using UnityEngine;
 
-public class PlayerEffectController : MonoBehaviour
+public class PlayerEffectController : NetworkBehaviour
 {
     [SerializeField] private ParticleSystem _punchEffect;
     [SerializeField] private Vector3 _stunEffectPositionOffset;
@@ -13,11 +14,26 @@ public class PlayerEffectController : MonoBehaviour
 
     public void PlayPunchEffect()
     {
-        var effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
-        if (effectSpawner && _punchEffect)
+        if (!HasStateAuthority) return; 
+        PlayPunchEffectLocal();
+        RPC_PlayPunchEffect();
+    }
+
+    // 全クライアントで実行されるRPC（送信者にも届く）
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_PlayPunchEffect()
+    {
+        PlayPunchEffectLocal();
+    }
+
+    // ここに実際の見た目処理を集約（従来の中身）
+    private void PlayPunchEffectLocal()
+    {
+        if (_punchEffect)
         {
             _punchFollow?.Dispose();
             _punchEffect.Play();
+
             _punchFollow = Observable.EveryLateUpdate()
                 .TakeWhile(_ => _punchEffect && _punchEffect.IsAlive(true) && _punchEffect.isPlaying)
                 .Subscribe(_ =>
