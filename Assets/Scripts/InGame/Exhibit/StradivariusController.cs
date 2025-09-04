4e5c6d4e3701fc5f5884c893c9895053c325ea30
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-09-04 22:00:44 +0900

    [Add]Effect生成処理の追加

--- Assets/Scripts/InGame/Exhibit/StradivariusController.cs
+++ Assets/Scripts/InGame/Exhibit/StradivariusController.cs
@@ -1,8 +1,10 @@
+using CRISound;
 using Fusion;
 using InGame.Health;
 using InGame.Player;
 using September.Common;
 using September.InGame.Common;
+using September.InGame.Effect;
 using UnityEngine;
 
 namespace InGame.Exhibit
@@ -10,11 +12,23 @@ namespace InGame.Exhibit
     public class StradivariusController : NetworkBehaviour
     {
         [SerializeField] private int _healAmount;
+        private EffectSpawner _effectSpawner;
+        [SerializeField] private Vector3 _offset;
         public void HealPlayer(PlayerRef playerRef)
         {
-             var playerHealth = StaticServiceLocator.Instance.Get<InGameManager>().PlayerDataDic[playerRef].GetComponent<PlayerHealth>();
+             var playerObject = StaticServiceLocator.Instance.Get<InGameManager>().PlayerDataDic[playerRef];
+             var playerHealth = playerObject.GetComponent<PlayerHealth>();
              var hitdata = new HitData(HitActionType.Heal,_healAmount, playerRef,playerRef);
              playerHealth.TakeHit(ref hitdata);
+             PlayEffect(playerObject);
+        }
+        
+        private void PlayEffect(NetworkObject netObj)
+        {
+            // Effect生成処理
+            _effectSpawner ??= StaticServiceLocator.Instance.Get<EffectSpawner>();
+            _effectSpawner?.RequestPlayOneShotEffect(EffectType.Heal, netObj.transform.position + _offset,
+                netObj.transform.rotation,netObj.transform);
         }
 
       
