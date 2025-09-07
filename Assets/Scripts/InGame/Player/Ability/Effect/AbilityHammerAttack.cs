1e657098e55c49611df8b038d8c259e8e378921d
TsuyoshiUsugi <qesulive@gmail.com>
2025-09-07 23:35:34 +0900

    ハンマー攻撃に関する処理

--- Assets/Scripts/InGame/Player/Ability/Effect/AbilityHammerAttack.cs
+++ Assets/Scripts/InGame/Player/Ability/Effect/AbilityHammerAttack.cs
@@ -1,56 +1,44 @@
+using System.Linq;
 using Fusion;
 using InGame.Common;
 using InGame.Exhibit.InteractEffect;
 using InGame.Health;
 using InGame.Interact;
 using September.Common;
+using September.InGame.Effect;
 using UnityEngine;
 
 namespace InGame.Player.Ability.Effect
 {
-    public class AbilityHammerAttack : AbilityBase
+    public class AbilityHammerAttack : AbilityNormalAttack
     {
-        [SerializeField] private AnimationClip _normalAttackAnimationClip;
-        [SerializeField] private LayerMask _hitMask;
-        [SerializeField] private HitChecker _hitChecker;
-        private int _attackDamage = int.MaxValue;
-        
-        protected override void OnStart()
+        protected override void OnHitEnemy(Collider hitInfo)
         {
-            var ownerAnimator = Parameter.Owner.GetComponent<AnimationClipPlayer>();
-            if (ownerAnimator && Parameter.Owner.HasInputAuthority)
-            {
-                //ownerAnimator.PlayClip(_normalAttackAnimationClip, 1, 0, true);
-            }
-            
-            _hitChecker.OnHit += OnHitEnemy;
-        }
-        
-        private void OnHitEnemy(Collider hitInfo)
-        {
+            Debug.Log(
+                $"Name {hitInfo.gameObject.name} networkObject {hitInfo.GetComponentInParent<NetworkObject>()} owner {Parameter.Owner} isAlive {hitInfo.GetComponentInParent<IDamageable>()?.IsAlive} disableInteractEffect {hitInfo.GetComponent<DisableInteractEffect>()}");
             if (hitInfo.GetComponentInParent<NetworkObject>() == Parameter.Owner) return;
             var damageable = hitInfo.GetComponentInParent<IDamageable>();
-            if (damageable == null) return;
-            var hitData = new HitData(
-                HitActionType.Damage,
-                _attackDamage,
-                Parameter.Owner.InputAuthority,
-                damageable.OwnerPlayerRef);
-            damageable.TakeHit(ref hitData);
-        }
+            var disableInteractEffect = hitInfo.GetComponent<DisableInteractEffect>();
+            if (damageable == null || !disableInteractEffect) return;
 
-        protected override void OnUpdate(float deltaTime)
-        {
-            if (_hitChecker.IsActive)
+            if (damageable.IsAlive)
             {
-                _phase = AbilityPhase.Ending;
+                var hitData = new HitData(
+                    HitActionType.Damage,
+                    _attackDamage,
+                    Parameter.Owner.InputAuthority,
+                    damageable.OwnerPlayerRef);
+                damageable.TakeHit(ref hitData);
             }
-        }
 
-        protected override void OnEndAbility()
-        {
-            _hitChecker.OnHit -= OnHitEnemy;
-            base.OnEndAbility();
+            if (disableInteractEffect)
+            {
+                disableInteractEffect.OnHitHammerAttack();
+            }
+
+            //エフェクトの再生
+            _effectSpawner.RequestPlayOneShotEffect(_hitEffect,
+                hitInfo.ClosestPoint(_hitChecker.HitPoint.First().position), Quaternion.identity);
         }
     }
 }
