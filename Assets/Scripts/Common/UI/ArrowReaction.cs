5bf1b9d919198a5fc7ee8bbbe384182d47cf6489
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2025-09-15 07:55:58 +0900

    [update] Title, LobbyにあるUI関係のScriptをNavigation対応にした。 [add] 選択中に矢印を表示する機能を追加した。

---
+++ Assets/Scripts/Common/UI/ArrowReaction.cs
@@ -0,0 +1,44 @@
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace September.Common
+{
+    public class ArrowReaction : MonoBehaviour
+    {
+        [SerializeField] private Image _arrowImage;
+        [SerializeField] private Selectable _targetSelectable;
+        Tween _tween;
+        protected void Awake()
+        {
+            if(_arrowImage) _arrowImage.enabled = false;
+            if (_targetSelectable)
+            {
+                _targetSelectable.OnSelectAsObservable().Subscribe(OnSelect).AddTo(this);
+                _targetSelectable.OnDeselectAsObservable().Subscribe(OnDeselect).AddTo(this);
+            }
+        }
+
+        private void OnSelect(BaseEventData eventData)
+        {
+            if (_arrowImage)
+            {
+                _arrowImage.enabled = true;
+                _tween = _arrowImage.transform.DORotate(new Vector3(360f, 0f, 0f), 1.5f, RotateMode.LocalAxisAdd).SetLoops(-1, LoopType.Restart).SetEase(Ease.InOutSine).SetLink(gameObject);
+            }
+        }
+
+        private void OnDeselect(BaseEventData eventData)
+        {
+            if (_arrowImage)
+            {
+                _arrowImage.enabled = false;
+                _arrowImage.transform.rotation = Quaternion.identity;
+                _tween?.Kill();
+            }
+        }
+    }
+}
--- Assets/Scripts/Lobby/ToggleTweenAnimation.cs
+++ Assets/Scripts/Lobby/ToggleTweenAnimation.cs
@@ -1,57 +1,71 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace September.Lobby
 {
     public class ToggleTweenAnimation : MonoBehaviour
     {
-        [SerializeField] Button _openButton;
-        [SerializeField] Button _closeButton;
-        [SerializeField] CanvasGroup _canvasGroup;
-        [SerializeField] RectOffsetData[] _rectOffsets;
-        [SerializeField] Ease _ease = Ease.OutBack; 
-        Vector2[] _initialPositions;
-
+        [SerializeField] private Selectable _selectWhenClose;
+        [SerializeField] private Button _openButton;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private RectOffsetData[] _rectOffsets;
+        [SerializeField] private Ease _ease = Ease.OutBack;
+        [SerializeField] private bool _startIsActive = true;
+        private Vector2[] _initialPositions;
+        private UniTask _currentTask;
+        public Selectable SelectWhenOpen { get; set; } = null;
         private void Awake()
         {
             _initialPositions = _rectOffsets.Select(r => r.RectTransform.anchoredPosition).ToArray();
-            _canvasGroup.alpha = 0;
-            _canvasGroup.interactable = false;
-            _canvasGroup.blocksRaycasts = false;
+            _canvasGroup.alpha = _startIsActive ? 1 : 0;
+            _canvasGroup.interactable = _startIsActive;
+            _canvasGroup.blocksRaycasts = _startIsActive;
             _openButton.onClick.AddListener(Open);
             _closeButton.onClick.AddListener(Close);
         }
 
         private void Open()
         {
-            _canvasGroup.DOFade(1, 1.5f).OnComplete(() =>
+            if (!_currentTask.Status.IsCompleted()) return;
+            List<UniTask> tasks = new();
+            tasks.Add(_canvasGroup.DOFade(1, 1.5f).OnComplete(() =>
             {
                 _canvasGroup.interactable = true;
                 _canvasGroup.blocksRaycasts = true;
-            });
+                if(SelectWhenOpen) EventSystem.current.SetSelectedGameObject(SelectWhenOpen.gameObject);
+            }).ToUniTask()); 
             for (int i = 0; i < _rectOffsets.Length; i++)
             {
                 var rectOffset = _rectOffsets[i];
                 rectOffset.RectTransform.anchoredPosition += rectOffset.StartOffset;
-                rectOffset.RectTransform.DOAnchorPos(_initialPositions[i], 2f).SetEase(_ease);
+                tasks.Add(rectOffset.RectTransform.DOAnchorPos(_initialPositions[i], 2f).SetEase(_ease).ToUniTask());
             }
+            _currentTask = UniTask.WhenAll(tasks);
         }
 
         private void Close()
         {
-            _canvasGroup.DOFade(0, 1.5f).OnComplete(() =>
+            if (!_currentTask.Status.IsCompleted()) return;
+            List<UniTask> tasks = new();
+            tasks.Add(_canvasGroup.DOFade(0, 1.5f).OnComplete(() =>
             {
                 _canvasGroup.interactable = false;
                 _canvasGroup.blocksRaycasts = false;
-            });
+                EventSystem.current.SetSelectedGameObject(_selectWhenClose.gameObject);
+            }).ToUniTask());
             for (int i = 0; i < _rectOffsets.Length; i++)
             {
                 var rectOffset = _rectOffsets[i];
-                rectOffset.RectTransform.DOAnchorPos(_initialPositions[i] + rectOffset.StartOffset, 2f).SetEase(_ease);
+                tasks.Add(rectOffset.RectTransform.DOAnchorPos(_initialPositions[i] + rectOffset.StartOffset, 2f).SetEase(_ease).ToUniTask());
             }
+            _currentTask = UniTask.WhenAll(tasks);
         }
         [Serializable]
         struct RectOffsetData
