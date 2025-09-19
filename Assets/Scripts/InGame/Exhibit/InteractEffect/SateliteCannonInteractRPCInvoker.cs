1bcf503e2ae70fd25ee495d75f815ab815eee28f
ShiomiKazuma <kazu_shio_1217@icloud.com>
2025-09-20 02:16:30 +0900

    [fix]モニターの修正

--- Assets/Scripts/InGame/Exhibit/InteractEffect/SateliteCannonInteractRPCInvoker.cs
+++ Assets/Scripts/InGame/Exhibit/InteractEffect/SateliteCannonInteractRPCInvoker.cs
@@ -1,6 +1,8 @@
 using System;
 using Fusion;
 using InGame.Interact;
+using InGame.Player;
+using UnityEngine;
 
 namespace Ingame.Exhibit
 {
@@ -8,10 +10,20 @@ namespace Ingame.Exhibit
     public class SateliteCannonInteractEffect : CharacterInteractEffectBase
     {
         public SateliteCannonInteractRPCInvoker SateliteCannonInteractRPCInvoker;
+        public Transform _lookPoint;
         public override void OnInteractStart(IInteractableContext context, InteractableBase target)
         {
             PlayerRef playerRef = PlayerRef.FromEncoded(context.Interactor);
             SateliteCannonInteractRPCInvoker.Rpc_RequestInteraction(playerRef);
+            //プレイヤーの向きを後ろにする
+            if (target.Runner.TryGetPlayerObject(playerRef, out NetworkObject playerNetworkObject))
+            {
+                Vector3 targetPos = _lookPoint.transform.position;
+                Vector3 direction = (targetPos - playerNetworkObject.transform.position).normalized;
+                Quaternion targetRot = Quaternion.LookRotation(direction, Vector3.up);
+                PlayerManager playerManager = playerNetworkObject.GetComponent<PlayerManager>();
+                playerManager?.SetWarpTarget(playerNetworkObject.transform.position, targetRot);
+            }
         }
 
         public override CharacterInteractEffectBase Clone()
@@ -19,6 +31,7 @@ namespace Ingame.Exhibit
             return new SateliteCannonInteractEffect
             {
                 SateliteCannonInteractRPCInvoker = SateliteCannonInteractRPCInvoker,
+                _lookPoint = _lookPoint,
             };
         }
     }
