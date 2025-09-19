d0448e82c7cc1b5461958d9d079fd6b3ec244eff
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-09-20 02:49:22 +0900

    [Fix]ティラノ複数乗れちゃう問題修正

--- Assets/Scripts/InGame/Exhibit/Tyranno/TyrannoInteractEffect.cs
+++ Assets/Scripts/InGame/Exhibit/Tyranno/TyrannoInteractEffect.cs
@@ -11,44 +11,39 @@ namespace InGame.Exhibit
     {
         [SerializeField] private float _interactTime;
         [SerializeField] private TyrannoInteractable _tyrannoInteractable;
-        
         private PlayerRef _ownerPlayerRef;
         private PlayerManager _ownerPlayerManager;
         private NetworkRunner _networkRunner;
-        private bool _isInteracting;
         private float _interactTimer;
         private InteractableBase _interactable;
 
         public override void OnInteractStart(IInteractableContext context, InteractableBase target)
         {
-            if(_isInteracting) return;
             _networkRunner = target.Runner;
             _interactable = target;
             var playerRef = PlayerRef.FromEncoded(context.Interactor);
             GetOn(playerRef);
-            
         }
+
         public override void OnInteractFixedNetworkUpdate(PlayerInput playerInput)
         {
-            if (_isInteracting)
+            _interactTimer += _networkRunner.DeltaTime;
+
+            if (playerInput.Buttons.IsSet(PlayerButtons.Interact) && _interactTimer > 1f)
             {
-                _interactTimer += _networkRunner.DeltaTime;
-                
-                if (playerInput.Buttons.IsSet(PlayerButtons.Interact) && _interactTimer > 1f)
-                {
-                    GetOff();
-                    return;
-                }
+                GetOff();
+                return;
             }
-            
+
+
             if (CheckInteractEnd())
             {
                 GetOff();
             }
-            _tyrannoInteractable.OnInteractFixedUpdate(playerInput,_networkRunner.DeltaTime);
+
+            _tyrannoInteractable.OnInteractFixedUpdate(playerInput, _networkRunner.DeltaTime);
         }
 
-   
         private bool CheckInteractEnd()
         {
             if (_interactTimer > _interactTime) return true;
@@ -59,18 +54,16 @@ namespace InGame.Exhibit
         {
             _ownerPlayerRef = ownerPlayerRef;
             _tyrannoInteractable.GetOn(ownerPlayerRef);
-            _isInteracting = true;
         }
 
         private void GetOff()
         {
             _tyrannoInteractable.GetOff(_ownerPlayerRef);
-            _isInteracting = false;
             _interactTimer = 0;
             _ownerPlayerRef = PlayerRef.None;
             _interactable.EndInteract();
         }
-        
+
         public override CharacterInteractEffectBase Clone()
         {
             return new TyrannoInteractEffect()
