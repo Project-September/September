d0af1802cb7a6ea804ad55b4cd103261184cf433
TsuyoshiUsugi <qesulive@gmail.com>
2025-09-21 15:54:39 +0900

    終了時に殴れないように

--- Assets/Scripts/InGame/Player/Ability/Condition/AbilityNormalAttackCondition.cs
+++ Assets/Scripts/InGame/Player/Ability/Condition/AbilityNormalAttackCondition.cs
@@ -2,6 +2,8 @@ using System;
 using Fusion;
 using InGame.Player.Ability;
 using September.Common;
+using September.InGame.Common;
+using UnityEngine;
 
 namespace  InGame.Player.Ability
 {
@@ -14,16 +16,35 @@ namespace  InGame.Player.Ability
 
         public bool IsConditionMatch(in TriggerEventContext context)
         {
+            Debug.LogError($"GameEnded: {IsGameEnded()}");
             if (!context.Owner) return false;
             if (!_playerMovement) _playerMovement = context.Owner.GetComponent<PlayerMovement>();
             if (!_playerManager) _playerManager = context.Owner.GetComponent<PlayerManager>();
 
+            
             //Available状態でAttackボタンが押されたら条件を満たす
-            return _playerMovement.IsGround && !_playerManager.IsStun &&
+            return _playerMovement.IsGround && !_playerManager.IsStun && !IsGameEnded() &&
                    _playerManager.CurrentPlayerControlState == PlayerManager.PlayerControlState.Normal &&
                    context.AbilityRef.Phase == AbilityBase.AbilityPhase.Available &&
                    context.AbilityRef.CanStartAbilityOverride() &&
                    context.CurrentButtons.GetPressed(context.PreviousButtons).IsSet(PlayerButtons.Attack);
+        }
+        
+        private bool IsGameEnded()
+        {
+            try
+            {
+                var inGameManager = StaticServiceLocator.Instance.Get<InGameManager>();
+                if (inGameManager == null) return false;
+
+                // EndingStateかどうかをチェック
+                return inGameManager.CurrentStateName == "EndingState";
+            }
+            catch (System.Exception)
+            {
+                // エラーが発生した場合は安全側にインタラクトを許可
+                return false;
+            }
         }
     }
 }
