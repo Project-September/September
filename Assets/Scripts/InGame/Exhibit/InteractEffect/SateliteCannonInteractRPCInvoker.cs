d63c338e8c1d0cdd1889138e445bea9a0d1ff08d
ShiomiKazuma <kazu_shio_1217@icloud.com>
2025-09-25 20:03:13 +0900

    [fix]サテライトキャノンインタラクト時のカメラ挙動を修正

--- Assets/Scripts/InGame/Exhibit/InteractEffect/SateliteCannonInteractRPCInvoker.cs
+++ Assets/Scripts/InGame/Exhibit/InteractEffect/SateliteCannonInteractRPCInvoker.cs
@@ -14,20 +14,7 @@ namespace Ingame.Exhibit
         public override void OnInteractStart(IInteractableContext context, InteractableBase target)
         {
             PlayerRef playerRef = PlayerRef.FromEncoded(context.Interactor);
-            SateliteCannonInteractRPCInvoker.Rpc_RequestInteraction(playerRef);
-            //プレイヤーの向きを後ろにする
-            if (target.Runner.TryGetPlayerObject(playerRef, out NetworkObject playerNetworkObject))
-            {
-                Vector3 targetPos = _lookPoint.transform.position;
-                Vector3 flatDirection = targetPos - playerNetworkObject.transform.position;
-                flatDirection.y = 0f; // 上下を無視して水平成分だけ
-                if (flatDirection.sqrMagnitude > 0.001f)
-                {
-                    Quaternion targetRot = Quaternion.LookRotation(flatDirection.normalized, Vector3.up);
-                    PlayerManager playerManager = playerNetworkObject.GetComponent<PlayerManager>();
-                    playerManager?.SetWarpTarget(playerNetworkObject.transform.position, targetRot);
-                }
-            }
+            SateliteCannonInteractRPCInvoker.Rpc_RequestInteraction(playerRef, _lookPoint.position);
         }
 
         public override CharacterInteractEffectBase Clone()
