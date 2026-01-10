9dd5970be55de57837ec345669fa2ab350e2980c
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-01-11 01:22:38 +0900

    [update] ResultSceneManagerクラスがテスト用データを参照しないように（このコミットはあとで改善する）
    1. テスト用データのためだけにScoreConfigに依存してしまっているので、そこを解消したい
    2. Awake, Startの実行順で左右されないようにしたい
    3. Domain ReloadがOffであっても問題なく動作するようにしたい

---
+++ Assets/Scripts/NewResult/Debug/MockData.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using September.Common;
+using UnityEngine;
+
+namespace September.NewResult
+{
+    public class MockData
+    {
+        private readonly RankingEntry[] _data;
+
+        [System.Serializable]
+        public struct RankingEntryData
+        {
+            public int _rank;
+            public string _playerName;
+            public CharacterType _characterType;
+
+            public RankingEntry Convert()
+            {
+                return new RankingEntry(_rank, _playerName, _characterType);
+            }
+        }
+        
+        public MockData(RankingEntryData[] data)
+        {
+            _data = data.Select(x => x.Convert()).ToArray();
+        }
+        
+        public GameResultInfo Create(ExhibitScoreConfig config)
+        {
+            var builder = new GameResultInfoBuilder();
+            builder.SetStageName("Field");
+            builder.SetRanking(_data);
+
+            var playerCount = _data.Length;
+            var ogrePlayerIndex = Random.Range(0, playerCount);
+            for (var i = 0; i < playerCount; i++)
+            {
+                var exhibitInteractCounts = 
+                    config.Entries
+                        .Select(x => new { key = x.Type, value = Random.Range(0, 10)})
+                        .ToDictionary(x => x.key, x => x.value);
+
+                var score = exhibitInteractCounts.Select((x, i) => x.Value * config.Entries[i].Points).Sum();
+                
+                var player = new PlayerResultInfoBuilder();
+                player.SetPlayerName(_data[i].PlayerName);
+                player.SetCharacterType((CharacterType)(i % 4 + 1));
+                player.SetTotalScore(score);
+                player.SetScoreConfig(config.Entries);
+                player.SetIsOgre(i == ogrePlayerIndex);
+                player.SetExhibitInteractCounts(exhibitInteractCounts);
+                builder.AddPlayer(player.BuildInstance());
+            }
+
+            var gameResultInfo = builder.BuildInstance();
+            return gameResultInfo;
+        }
+    }
+}
--- Assets/Scripts/NewResult/Manager/ResultSceneManager.cs
+++ Assets/Scripts/NewResult/Manager/ResultSceneManager.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using September.Common;
 using UnityEngine;
 
 namespace September.NewResult
@@ -9,56 +7,14 @@ namespace September.NewResult
         [SerializeField] private ResultCharacterDataContainer _resultCharacterDataContainer;
         [SerializeField] private ResultPerformanceManager _resultPerformanceManager;
         [SerializeField] private ResultUIInitializer _resultUIInitializer;
-        [SerializeField] private ExhibitScoreConfig _config;
 
         private async void Start()
         {
-            var gameResultInfo = InGameResultContainer.Info ?? MockData.Create(_config);
+            var gameResultInfo = InGameResultContainer.Info;
             
             _resultUIInitializer.Initialize(gameResultInfo);
             
             await _resultPerformanceManager.StartResultPerformance(_resultCharacterDataContainer, gameResultInfo);
-        }
-    }
-
-    public static class MockData
-    {
-        public static GameResultInfo Create(ExhibitScoreConfig config)
-        {
-            var builder = new GameResultInfoBuilder();
-            builder.SetStageName("Field");
-            builder.SetRanking(new RankingEntry[]
-            {
-                new(2, "Second Octane", CharacterType.HulkTheButcher),
-                new(1, "First the Winner with Long Name", CharacterType.OkabeWright),
-                new(3, "Thirdlongestlongestlongestname", CharacterType.OkabeWright),
-                new(5, "五位あいうえおかきくけこさしすせそ", CharacterType.Sarutobi),
-                new(4, "Fourth long long long long Name", CharacterType.Tanihira),
-            });
-
-            var playerCount = Random.Range(1, 6);
-            var ogrePlayerIndex = Random.Range(0, playerCount);
-            for (var i = 0; i < playerCount; i++)
-            {
-                var exhibitInteractCounts = 
-                    config.Entries
-                        .Select(x => new { key = x.Type, value = Random.Range(0, 10)})
-                        .ToDictionary(x => x.key, x => x.value);
-
-                var score = exhibitInteractCounts.Select((x, i) => x.Value * config.Entries[i].Points).Sum();
-                
-                var player = new PlayerResultInfoBuilder();
-                player.SetPlayerName($"Test_Player{i + 1}");
-                player.SetCharacterType((CharacterType)(i % 4 + 1));
-                player.SetTotalScore(score);
-                player.SetScoreConfig(config.Entries);
-                player.SetIsOgre(i == ogrePlayerIndex);
-                player.SetExhibitInteractCounts(exhibitInteractCounts);
-                builder.AddPlayer(player.BuildInstance());
-            }
-
-            var gameResultInfo = builder.BuildInstance();
-            return gameResultInfo;
         }
     }
 }
