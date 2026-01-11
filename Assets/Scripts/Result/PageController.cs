6d92dedff9736ef7a8f5f2d1279b4e020738c86c
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-01-11 16:46:33 +0900

    [update] 不要になった処理を削除

--- Assets/Scripts/Result/PageController.cs
+++ Assets/Scripts/Result/PageController.cs
@@ -98,59 +98,9 @@ namespace Result
             if (success)
             {
                 SceneManager.LoadSceneAsync("NewResult");
-                return BuildGameResultInfo();
             }
             return false;
         }
-
-        private bool BuildGameResultInfo()
-        {
-            var builder = new GameResultInfoBuilder();
-                
-            var db = PlayerDatabase.Instance;
-            if (!db)
-                return false;
-
-            var data = db.PlayerDataDic
-                .Select(kv =>
-                {
-                    SessionPlayerData d = kv.Value;
-                    return new {
-                        name = d.DisplayNickName,
-                        score = d.Score,
-                        isOgre = (bool)d.IsOgre,
-                        type = d.CharacterType,
-                    };
-                }).ToArray();
-
-            var ogres = data.Where(x => x.isOgre).OrderByDescending(x => x.score);
-            var nonOgres = data.Where(x => x.isOgre == false).OrderByDescending(x => x.score);
-            var ranking = nonOgres.Concat(ogres).ToArray();
-                
-            for (var i = 0; i < ranking.Length; i++)
-            {
-                var rank = i + 1;
-                var playerName = ranking[i].name;
-                var characterType = ranking[i].type;
-                var score = ranking[i].score;
-                var isOgre = ranking[i].isOgre;
-                    
-                builder.AddRankingEntry(new RankingEntry(rank, playerName, characterType));
-                
-                var player = new PlayerResultInfoBuilder();
-                player.SetPlayerName(playerName);
-                player.SetCharacterType(characterType);
-                player.SetTotalScore(score);
-                player.SetIsOgre(isOgre);
-                
-                builder.AddPlayer(player.BuildInstance());
-            }
-                
-            builder.SetStageName("Field");
-            InGameResultContainer.Set(builder.BuildInstance());
-            return true;
-        }
-
         public void Initialize()
         {
             _gameInput = GameInput.I;
