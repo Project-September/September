8ca1cc415882b83e1a173b983b5b43948ed0844c
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-02-20 18:25:54 +0900

    [update] ボイスの再生位置を指定しないように

--- Assets/Scripts/NewResult/Audio/AudioController.cs
+++ Assets/Scripts/NewResult/Audio/AudioController.cs
@@ -1,4 +1,3 @@
-using System;
 using CRISound;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -7,17 +6,11 @@ namespace September.NewResult
 {
     public class AudioController : MonoBehaviour, INotificationReceiver
     {
-        [SerializeField] private Transform _transform;
         [SerializeField] private string _cueSheet = "ALLCue";
-
-        private void Start()
-        {
-            if (_transform == null) _transform = transform;
-        }
 
         private void PlaySound(string cueName)
         {
-            CRIAudio.PlaySE(_transform.position, _cueSheet, cueName);
+            CRIAudio.PlaySE(_cueSheet, cueName);
         }
 
         public void OnNotify(Playable origin, INotification notification, object context)
