38c82c8a07cfcdc154b8f31d7e9244ad2c88e309
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-03-27 19:41:46 +0900

    [update] 必殺技発動中にVFXを発生させる処理を追加。必殺技の長さを設定可能にした

--- Assets/Scripts/InGame/Player/Ability/Effect/AbilityOkabeUlt.cs
+++ Assets/Scripts/InGame/Player/Ability/Effect/AbilityOkabeUlt.cs
@@ -1,7 +1,7 @@
 using System;
 using InGame.Common;
 using September.Common;
-using September.InGame.Common;
+using September.InGame.Effect;
 using UnityEngine;
 
 namespace InGame.Player.Ability.Effect
@@ -9,19 +9,43 @@ namespace InGame.Player.Ability.Effect
     [Serializable]
     public class AbilityOkabeUlt : AbilityBase
     {
+        [SerializeField] private float _duration = 5f;
         [SerializeField] private StatusEffect _buffEffect;
+        [SerializeField] private EffectType _effectType;
         
+        protected EffectSpawner _effectSpawner;
+        private string _effectID;
+
         protected override void OnStart()
         {
-            Debug.Log("[AbilityUlt] OnStart");
-            var manager = StaticServiceLocator.Instance.Get<InGameManager>();
-            var player = manager.PlayerDataDic[manager.Runner.LocalPlayer];
+            Debug.Log("[AbilityOkabeUlt] Start");
+            var player = Parameter.Owner;
 
             if (player.TryGetComponent<PlayerStatus>(out var playerStatus))
             {
-                EffectableStatus.StatusEffectSpec spec = new EffectableStatus.StatusEffectSpec(_buffEffect);
+                var spec = new EffectableStatus.StatusEffectSpec(_buffEffect);
+                spec.Duration = _duration;
                 playerStatus.AddEffect(spec);
             }
+            
+            _effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
+            _effectID = Guid.NewGuid().ToString();
+            _effectSpawner.RequestPlayLoopEffect(_effectID, _effectType, player.transform.position, Quaternion.identity, player.transform);
+        }
+
+        protected override void OnUpdate(float deltaTime)
+        {
+            var elapsedTick = Runner.Tick - StartTick;
+            if (elapsedTick >= _duration / Runner.DeltaTime)
+            {
+                RequestEndAbility();
+            }
+        }
+
+        protected override void OnEndAbility()
+        {
+            Debug.Log($"[AbilityOkabeUlt] End {StartTick} {Runner.Tick} {Runner.Tick - StartTick} {(Runner.Tick - StartTick) * Runner.DeltaTime}");
+            _effectSpawner.StopEffect(_effectID);
         }
     }
 }
