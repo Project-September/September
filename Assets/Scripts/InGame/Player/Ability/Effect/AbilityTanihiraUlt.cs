3108c3185901ed54f405e91e526ac33486cdc569
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-04-05 03:08:06 +0900

    [update, add] タニヒラ必殺技実装（ペンギン増殖）

---
+++ Assets/Scripts/InGame/Player/Ability/Effect/AbilityTanihiraUlt.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Ingame.Tanihira;
+using InGame.Tanihira;
+using UnityEngine;
+
+namespace InGame.Player.Ability.Effect
+{
+    [Serializable]
+    public class AbilityTanihiraUlt : AbilityUltBase
+    {
+        [SerializeField] private float _duration = 10;
+        [SerializeField] private int _spawnCount = 3;
+        
+        private FriendsSpawner _friendsSpawner;
+        private FormationManager _formationManager;
+        private List<FriendBase> _spawnedFriends = new();
+        
+        protected override void OnCutInEnd()
+        {
+            var player = Parameter.Owner;
+
+            if (!_friendsSpawner) _friendsSpawner = player.GetComponent<FriendsSpawner>();
+            if (!_formationManager) _formationManager = player.GetComponent<FormationManager>();
+
+            for (int i = 0; i < _spawnCount; i++)
+            {
+                var friend = _friendsSpawner.SpawnFriend(FriendType.ChildPenguin, player.transform);
+                _spawnedFriends.Add(friend);
+            }
+        }
+
+        protected override void OnUpdate(float deltaTime)
+        {
+            if (Runner.SimulationTime - StartTime > _duration)
+            {
+                RequestEndAbility();
+            }
+        }
+
+        protected override void OnEndAbility()
+        {
+            foreach (var friend in _spawnedFriends)
+            {
+                _formationManager.DeleteFriend(friend);
+                friend.gameObject.SetActive(false);
+            }
+            _spawnedFriends.Clear();
+        }
+    }
+}
