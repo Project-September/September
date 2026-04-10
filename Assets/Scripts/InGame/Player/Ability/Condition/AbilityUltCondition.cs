ed3e84395ee8108577fa25cd03f4a3324d87ee65
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-04-10 18:25:17 +0900

    [update] 必殺技発動中に再発動できないように

--- Assets/Scripts/InGame/Player/Ability/Condition/AbilityUltCondition.cs
+++ Assets/Scripts/InGame/Player/Ability/Condition/AbilityUltCondition.cs
@@ -11,10 +11,19 @@ namespace InGame.Player.Ability.Condition
     {
         [SerializeField] private string _targetAbilityName;
         [SerializeField] private PlayerButtons _button = PlayerButtons.Ability3;
+        
+        private PlayerManager _playerManager;
+        
         public string TargetAbilityName => _targetAbilityName;
         
         public bool IsConditionMatch(in TriggerEventContext context)
         {
+            if (!_playerManager) _playerManager = context.Owner.GetComponent<PlayerManager>();
+            
+            if (context.AbilityRef.Phase != AbilityBase.AbilityPhase.Available
+                || _playerManager.CurrentPlayerControlState != PlayerManager.PlayerControlState.Normal
+                || _playerManager.IsStun) return false;
+            
             if (!context.CurrentButtons.GetPressed(context.PreviousButtons).IsSet(_button) ||
                 !PlayerDatabase.Instance.PlayerDataDic.TryGet(NetworkRunner.Instances[0].LocalPlayer, out _))
                 return false;
