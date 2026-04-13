f92c24b3291a8192c0499070aa35fbaea293f7a0
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-04-14 01:49:35 +0900

    [fix] 必殺技発動までの残りスコアや進行度がクライアントに共有されるように

--- Assets/Scripts/InGame/Player/Ult/UltCondition.cs
+++ Assets/Scripts/InGame/Player/Ult/UltCondition.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 namespace InGame.Player.Ult
 {
-    public class UltCondition : MonoBehaviour, IUltCondition
+    public class UltCondition : NetworkBehaviour, IUltCondition
     {
         [SerializeField] private int _requiredScore = 1000;
 
         /// <summary> 直近の必殺技発動時のスコア </summary>
-        private int _prevScore;
+        [Networked] private int PrevScore { get; set; }
         
         private int _currentScore;
         
-        public int RemainingScore => Mathf.Clamp(_requiredScore - (_currentScore - _prevScore), 0, _requiredScore);
-        public float Progress => Mathf.Clamp01((float)(_currentScore - _prevScore) / _requiredScore);
+        public int RemainingScore => Mathf.Clamp(_requiredScore - (_currentScore - PrevScore), 0, _requiredScore);
+        public float Progress => Mathf.Clamp01((float)(_currentScore - PrevScore) / _requiredScore);
 
         public bool IsAvailable()
         {
-            return _currentScore - _prevScore >= _requiredScore;
+            return _currentScore - PrevScore >= _requiredScore;
         }
 
         public void OnUltActivated()
         {
-            _prevScore = _currentScore;
+            PrevScore = _currentScore;
         }
         
         private void Start()
@@ -31,15 +31,7 @@ namespace InGame.Player.Ult
             // スコアの変動を監視
             PlayerDatabase.Instance.ChangedDataAction += dict =>
             {
-                var runner = NetworkRunner.Instances[0];
-                if (runner == null)
-                {
-                    Debug.LogError("[UltCondition] NetworkRunner is not initialized");
-                    return;
-                }
-                
-                var localPlayer = runner.LocalPlayer;
-                if (!dict.TryGet(localPlayer, out var playerData))
+                if (!dict.TryGet(Runner.LocalPlayer, out var playerData))
                 {
                     Debug.LogError("[UltCondition] PlayerData is not found");
                     return;
