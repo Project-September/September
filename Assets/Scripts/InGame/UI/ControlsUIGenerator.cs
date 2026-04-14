ecb384ce51137cbc73a1764fa3e783ee8db1a3d8
KRYG <abcdef0509a@gmail.com>
2026-04-14 21:35:26 +0900

    [add]操作説明UIを生成するクラス

---
+++ Assets/Scripts/InGame/UI/ControlsUIGenerator.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsUIGenerator : MonoBehaviour
+{
+    // 操作説明を生成した際にそのオブジェクトの親になるオブジェクト
+    // VerticalLayoutGroup,ContentSizeFitterなどのレイアウトコンポーネントがアタッチされていることを想定
+    [SerializeField] private GameObject _descriptionObject;
+    [SerializeField] private ControlDescription _descriptionData;
+    [SerializeField] private GameObject _iconPrefub;
+
+    private void Start()
+    {
+        GenerateDescription(_descriptionData);
+    }
+
+    /// <summary>
+    /// ControlDescriptionの内容をもとに操作説明UIを生成する
+    /// </summary>
+    public void GenerateDescription(ControlDescription description)
+    {   
+        foreach (var action in description.Actions)
+        {
+            GameObject icon = Instantiate(_iconPrefub, _descriptionObject.transform);
+            icon.name = $"{action.ActionName}Icon";
+            icon.GetComponent<Image>().sprite = action.Icon;
+            icon.GetComponentInChildren<TextMeshProUGUI>().text = action.Description;
+        }
+    }
+}
