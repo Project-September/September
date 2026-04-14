04120af8311243b6b11cd97eb6694ccfc8da6ecd
KRYG <abcdef0509a@gmail.com>
2026-04-14 20:06:16 +0900

    [add]操作説明UI用のScriptableObjectを追加

---
+++ Assets/Scripts/InGame/UI/ControlDescription.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+/// <summary>
+/// 操作説明を格納するデータ
+/// </summary>
+[CreateAssetMenu(fileName = "Description", menuName = "Scriptable Objects/ControlDescriptionUI")]
+public class ControlDescription : ScriptableObject
+{
+    public List<DescribedAction> Actions;
+}
+
+/// <summary>
+/// 各アクションの説明
+/// </summary>
+[System.Serializable]
+public class DescribedAction
+{
+    public string ActionName;
+    public InputActionReference Action;
+    public Sprite Icon;
+    public string Description;
+}
