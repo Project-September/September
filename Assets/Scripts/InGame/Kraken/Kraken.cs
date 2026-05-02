ce53611ca805070be15e8f0e538931ed6728916c
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-05-02 19:32:18 +0900

    [update] クラーケン攻撃テスト

--- Assets/Scripts/InGame/Kraken/Kraken.cs
+++ Assets/Scripts/InGame/Kraken/Kraken.cs
@@ -1,4 +1,7 @@
+using System;
+using Cysharp.Threading.Tasks;
 using Fusion;
+using InGame.Health;
 using InGame.Player;
 using September.Common;
 using UnityEngine;
@@ -11,10 +14,41 @@ namespace September.InGame.Kraken
         private const int CameraPriority = 15;
         
         /// <summary> クラーケン時カメラのコントローラー </summary>
+        [Header("カメラ")]
         [SerializeField] private CameraController _cameraController;
         
-        [Networked] private float Rotate { get; set; }
+        /// <summary> 攻撃処理コンポーネント </summary>
+        [Header("攻撃設定")]
+        [SerializeField] private HitChecker _hitChecker;
+        [SerializeField] private float _hitStartTime;
+        [SerializeField] private float _hitEndTime;
+        [SerializeField] private int _damage;
+
+        [Header("アニメーション設定")] 
+        [SerializeField] private Transform _leg;
+        [SerializeField] private Animator _animator;
         
+        [Networked] private float Rotate { get; set; }
+
+        private void Start()
+        {
+            _hitChecker.OnHit += x =>
+            {
+                if (x.TryGetComponent<IDamageable>(out var damageable))
+                {
+                    var hitData = new HitData()
+                    {
+                        HitActionType = HitActionType.Damage,
+                        Amount = _damage,
+                        ExecutorRef = Object.InputAuthority,
+                        TargetRef = damageable.OwnerPlayerRef,
+                    };
+                    
+                    damageable.TakeHit(ref hitData);
+                }
+            };
+        }
+
         /// <summary>
         /// ローカルでの入力処理
         /// </summary>
@@ -32,6 +66,28 @@ namespace September.InGame.Kraken
         }
         
         /// <summary>
+        /// ネットワーク上での入力処理
+        /// </summary>
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput<PlayerInput>(out var input))
+            {
+                Rotate += input.MoveDirection.x;
+
+                if (input.Buttons.IsSet(PlayerButtons.Attack))
+                {
+                    if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out var hit))
+                    {
+                        var targetPos = hit.point;
+                        Attack(targetPos).Forget();
+                    }
+                }
+            }
+            
+            transform.Rotate(0, Rotate, 0);
+        }
+        
+        /// <summary>
         /// 指定のプレイヤーをクラーケンにする
         /// </summary>
         /// <param name="owner"></param>
@@ -46,23 +102,20 @@ namespace September.InGame.Kraken
             
             // 元のプレイヤーオブジェクトを取得
             var playerObject = Runner.GetPlayerObject(owner);
+            if (playerObject == null) return;
             
             // 元のプレイヤーオブジェクトを非表示にする
             var playerManager = playerObject.GetComponent<PlayerManager>();
             if (playerManager != null) playerManager.RPC_SetInvisible(true);
         }
 
-        /// <summary>
-        /// ネットワーク上での入力処理
-        /// </summary>
-        public override void FixedUpdateNetwork()
+        public async UniTask Attack(Vector3 targetPos)
         {
-            if (GetInput<PlayerInput>(out var input))
-            {
-                Rotate += input.MoveDirection.x;
-            }
-            
-            transform.Rotate(0, Rotate, 0);
+            _leg.position = targetPos;
+            await UniTask.WaitForSeconds(_hitStartTime);
+            _hitChecker.StartHitCheck();
+            await UniTask.WaitForSeconds(_hitEndTime - _hitStartTime);
+            _hitChecker.EndHitCheck();
         }
     }
 }
