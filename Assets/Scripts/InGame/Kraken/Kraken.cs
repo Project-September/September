a7ab8ef6e8142ca9e4fafd5434e64b28d59405a7
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-05-02 18:47:15 +0900

    [update] コメント追加

--- Assets/Scripts/InGame/Kraken/Kraken.cs
+++ Assets/Scripts/InGame/Kraken/Kraken.cs
@@ -7,37 +7,54 @@ namespace September.InGame.Kraken
 {
     public class Kraken : NetworkBehaviour
     {
+        /// <summary> クラーケン中のカメラ優先度 </summary>
         private const int CameraPriority = 15;
         
+        /// <summary> クラーケン時カメラのコントローラー </summary>
         [SerializeField] private CameraController _cameraController;
         
         [Networked] private float Rotate { get; set; }
         
+        /// <summary>
+        /// ローカルでの入力処理
+        /// </summary>
         private void LateUpdate()
         {
+            // ローカル以外は弾く
             if(!HasInputAuthority) return;
             
             if (GameInput.I.Player.Aim.triggered)
             {
                 _cameraController.CameraReset();
             }
+            
             _cameraController.RotateCamera(GameInput.I.Player.Look.ReadValue<Vector2>(),Runner.DeltaTime);
         }
         
+        /// <summary>
+        /// 指定のプレイヤーをクラーケンにする
+        /// </summary>
+        /// <param name="owner"></param>
         public void GetOn(PlayerRef owner)
         {
+            // カメラを有効化する
             if (HasInputAuthority)
             {
                 _cameraController.Init(true);
                 _cameraController.SetCameraPriority(CameraPriority);
             }
             
+            // 元のプレイヤーオブジェクトを取得
             var playerObject = Runner.GetPlayerObject(owner);
             
+            // 元のプレイヤーオブジェクトを非表示にする
             var playerManager = playerObject.GetComponent<PlayerManager>();
             if (playerManager != null) playerManager.RPC_SetInvisible(true);
         }
 
+        /// <summary>
+        /// ネットワーク上での入力処理
+        /// </summary>
         public override void FixedUpdateNetwork()
         {
             if (GetInput<PlayerInput>(out var input))
