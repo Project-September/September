4c6cd7fe702b13044ed83bf4e6d50ce0c56d8a61
tomo-1230 <satokawa1130tomoki2@gmail.com>
2026-05-03 15:45:38 +0900

    [fix] 動けなくなったら再計算する

--- Assets/Scripts/InGame/Bot/State/BotStateMachine.cs
+++ Assets/Scripts/InGame/Bot/State/BotStateMachine.cs
@@ -5,11 +5,16 @@ namespace InGame.Bot
 {
     public class BotStateMachine : MonoBehaviour
     {
+        [SerializeField] private float _stopAmount;
+        [SerializeField] private float _stopTime;
+        [SerializeField] private Rigidbody _rigidbody;
         [field:SerializeField] public float _stopDistance { get; private set; }
         private IBotState _currentState;
         private RandomMoveState _randomState = new();
 
         public Vector3 _direction;
+        public bool _vault;
+        private float _stopTimer;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -19,6 +24,20 @@ namespace InGame.Bot
         void Update()
         {
             _currentState?.OnUpdate(this);
+
+            if(_rigidbody.linearVelocity.magnitude < _stopAmount)
+            {
+                _stopTimer -= Time.deltaTime;
+                if(_stopTimer < 0 )
+                {
+                    Debug.Log("ƒtƒŠ[ƒY");
+                    _currentState?.OnEnter(this);
+                }
+            }
+            else
+            {
+                _stopTimer = _stopTime;
+            }
         }
         public void ChangeState(IBotState state)
         {
@@ -30,7 +49,7 @@ namespace InGame.Bot
         {
             PlayerInput input = new();
             input.MoveDirection = _direction.normalized;
-
+            input.Buttons.Set(PlayerButtons.Jump, true);
             return input;
         }
     }
