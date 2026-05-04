dbf607c9bd50edb4e7f8dc4615677433fc9354d9
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-05-05 01:41:23 +0900

    [update] ステータス定義データを配列に変更

--- Assets/Scripts/InGame/Common/Stats/Player/PlayerParameter.cs
+++ Assets/Scripts/InGame/Common/Stats/Player/PlayerParameter.cs
@@ -1,4 +1,3 @@
-using System;
 using September.InGame.Common.Stats;
 using UnityEngine;
 
@@ -10,33 +9,11 @@ namespace InGame.Player
     [CreateAssetMenu(fileName = "PlayerParameter", menuName = "Scriptable Objects/Player/PlayerParameter")]
     public class PlayerParameter : ScriptableObject
     {
-        [SerializeField] int _health;
-        [SerializeField] float _speed;
-        [SerializeField] float _stamina;
-        [SerializeField] float _staminaConsumption;
-        [SerializeField] float _staminaRegen;
-        [SerializeField] int _attackDamage;
+        [SerializeField] Stat[] _stats;
         
-        public int Health => _health;
-        public float Speed => _speed;
-        public float Stamina => _stamina;
-        public float StaminaRegen => _staminaRegen;
-        public float StaminaConsumption => _staminaConsumption;
-        public int AttackDamage => _attackDamage;
-
         public StatsContainer GetStats()
         {
-            Span<Stat> stats = stackalloc Stat[]
-            {
-                new(StatType.Health, Health, maxValue: Health),
-                new(StatType.Speed, Speed),
-                new(StatType.Stamina, Stamina, maxValue: Stamina),
-                new(StatType.StaminaRegen, StaminaRegen),
-                new(StatType.StaminaConsumption, StaminaConsumption),
-                new(StatType.AttackDamage, AttackDamage),
-            };
-            
-            return new StatsContainer(stats);
+            return new StatsContainer(_stats);
         }
     }
 }
