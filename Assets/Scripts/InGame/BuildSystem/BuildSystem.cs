c6ab2f0172aefe41f0ea2b940d493acb7067c14a
KNTy1018 <soramametaro1018@gmail.com>
2026-05-06 18:20:07 +0900

    [add]インゲームのビルドシステム用スクリプト作成

---
+++ Assets/Scripts/InGame/BuildSystem/BuildSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace September.InGame.Common.Stats
+{
+    public class BuildSystem : StatsEffectorBase
+    {
+        public override void Apply(ref StatsContainer stats)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
