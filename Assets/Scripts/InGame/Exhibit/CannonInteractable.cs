c32ba8bf66fec94d4c2eeac2580bff63630f4ba5
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2026-06-09 04:24:10 +0900

    fix:一部のネットワーク同期がうまく動かなかった部分を修正

--- Assets/Scripts/InGame/Exhibit/CannonInteractable.cs
+++ Assets/Scripts/InGame/Exhibit/CannonInteractable.cs
@@ -22,8 +22,8 @@ namespace InGame.Exhibit
 
 		[Header("描画")] [SerializeField] private Transform _aimHitViewObject;
 		[SerializeField] private LineRenderer _lineRenderer;
-		[SerializeField] private GameObject _fireParticlePrefab;
-		[SerializeField] private GameObject _explosionParticlePrefab;
+		[SerializeField] private NetworkObject _fireParticlePrefab;
+		[SerializeField] private NetworkObject _explosionParticlePrefab;
 
 		[Header("射撃")] [SerializeField] private LayerMask _playerHitMask;
 		[SerializeField] private float _simulationStepTime = 0.1f;
@@ -43,34 +43,92 @@ namespace InGame.Exhibit
 		private Quaternion _baseDefaultRotation;
 		private Quaternion _barrelDefaultRotation;
 		private int _currentAmmo;
+		private Vector3[] _linePositions = new Vector3[32];
+		private NetworkObject _fireParticle;
+		private NetworkObject _explosionParticle;
+		private NetworkObject _aimObj;
+
+		[Networked] private Quaternion _baseRotation { get; set; }
+		[Networked] private Quaternion _barrelRotation { get; set; }
 
 		[Networked] public bool IsActive { get; private set; }
 		[Networked] private TickTimer LastFireTime { get; set; }
-		[Networked] [Capacity(32)] private NetworkArray<Vector3> _linePositions => default;
 		[Networked] private int _lastPositionIndex { get; set; }
 
 		#region Networked Methods
+
 		public override void Spawned()
 		{
 			base.Spawned();
 			_baseDefaultRotation = _cannonBase.localRotation;
 			_barrelDefaultRotation = _cannonBarrel.localRotation;
+			_baseRotation = _cannonBase.localRotation;
+			_barrelRotation = _cannonBarrel.localRotation;
+			_fireParticle = Runner.Spawn(_fireParticlePrefab, Vector3.zero, Quaternion.identity);
+			_explosionParticle = Runner.Spawn(_explosionParticlePrefab, Vector3.zero, Quaternion.identity);
 		}
 
 		public override void Render()
 		{
 			base.Render();
 			CreateLine();
+			_cannonBarrel.localRotation = _barrelRotation;
+			_cannonBase.localRotation = _baseRotation;
 		}
 
 
-		public override void FixedUpdateNetwork()
+		public void OnInteractFixedNetworkUpdate(PlayerInput input)
 		{
 			base.FixedUpdateNetwork();
-			if (!GetInput<PlayerInput>(out var input)) return;
-			OnInteractFixedNetworkUpdate(input);
+			//if (!GetInput<PlayerInput>(out var input)) return;
+
+			// キャノンの回転処理
+			CannonRotate(input.MoveDirection.x, input.LookDirection.y);
+			_currentUsePlayer?.SetWarpTarget(_waitCharacterTransform.position, _waitCharacterTransform.rotation);
+
+			// 射撃処理
+			if (input.Buttons.IsSet(PlayerButtons.Attack) && LastFireTime.ExpiredOrNotRunning(Runner) &&
+			    _currentAmmo > 0)
+			{
+				RPC_Fire();
+				LastFireTime = TickTimer.CreateFromSeconds(Runner, _reloadTime);
+				_currentAmmo -= 1;
+				if (_currentAmmo <= 0) Invoke(nameof(OnInteractEnd), .1f);
+			}
 		}
+
 		#endregion
+		
+		/// <summary>
+		///     インタラクト開始時の初期化処理
+		/// </summary>
+		public void OnInteractStart(PlayerRef playerRef)
+		{
+			IsActive = true;
+			Object.AssignInputAuthority(playerRef);
+			_currentAmmo = _maxAmmo;
+
+			// プレイヤーの取得
+			_currentUsePlayer = Runner.GetPlayerObject(playerRef).GetComponent<PlayerManager>();
+			if (_currentUsePlayer == null) return;
+			RPC_PlayerUnActive();
+			_currentUsePlayer.SetWarpTarget(_waitCharacterTransform.position, _waitCharacterTransform.rotation);
+		}
+
+		/// <summary>
+		///     インタラクト終了時の処理
+		/// </summary>
+		public void OnInteractEnd()
+		{
+			IsActive = false;
+			Refresh();
+			if (_currentUsePlayer == null) return;
+			Object.RemoveInputAuthority();
+			PlayerActive(true);
+			_currentUsePlayer = null;
+
+			IsActive = false;
+		}
 
 
 		private void Refresh()
@@ -85,7 +143,7 @@ namespace InGame.Exhibit
 			if (!IsActive) return;
 			BuildTrajectory(); // 描画位置の計算
 			_lineRenderer.positionCount = _lastPositionIndex + 1;
-			_lineRenderer.SetPositions(_linePositions.ToArray());
+			_lineRenderer.SetPositions(_linePositions);
 
 			var endPos = _linePositions[_lastPositionIndex];
 			_aimHitViewObject.position = endPos;
@@ -93,9 +151,9 @@ namespace InGame.Exhibit
 		}
 
 		/// <summary>
-		///   放物線の軌道を計算し、当たり判定を取得する。
+		///     放物線の軌道を計算し、当たり判定を取得する。
 		///     障害物に当たった場合、そこを最終地点とする。
-		///		結果は_linePositionsと_lastPositionIndexに保存される。
+		///     結果は_linePositionsと_lastPositionIndexに保存される。
 		/// </summary>
 		private void BuildTrajectory()
 		{
@@ -105,7 +163,7 @@ namespace InGame.Exhibit
 				var time = i * _simulationStepTime;
 				var pos = TrajectoryCalculator(_muzzle.position,
 					_muzzle.transform.forward * _ballSpeed, 9.8f, time);
-				_linePositions.Set(i, pos);
+				_linePositions[i] = pos;
 
 				if (i == 0) continue;
 				// 当たり判定の取得
@@ -115,7 +173,7 @@ namespace InGame.Exhibit
 				if (Physics.Raycast(ray, out var hit, Vector3.Distance(_linePositions[i - 1], _linePositions[i])))
 				{
 					_lineRenderer.positionCount = i;
-					_linePositions.Set(i, hit.point);
+					_linePositions[i] = hit.point;
 					_lastPositionIndex = i;
 					return;
 				}
@@ -124,70 +182,33 @@ namespace InGame.Exhibit
 			_lastPositionIndex = _linePositions.Length - 1;
 		}
 
-
-		/// <summary>
-		///     インタラクト開始時の初期化処理
-		/// </summary>
-		public void OnInteractStart(PlayerRef playerRef)
-		{
-			IsActive = true;
-			Object.AssignInputAuthority(playerRef);
-			_currentAmmo = _maxAmmo;
-
-			// プレイヤーの取得
-			_currentUsePlayer = Runner.GetPlayerObject(playerRef).GetComponent<PlayerManager>();
-			if (_currentUsePlayer == null) return;
-			PlayerActive(false);
-			_currentUsePlayer.SetWarpTarget(_waitCharacterTransform.position, _waitCharacterTransform.rotation);
-		}
-
-		/// <summary>
-		///     インタラクト終了時の処理
-		/// </summary>
-		public void OnInteractEnd()
-		{
-			if (_currentUsePlayer == null) return;
-			Object.RemoveInputAuthority();
-			PlayerActive(true);
-			_currentUsePlayer = null;
-
-			Refresh();
-			IsActive = false;
-		}
-
 		private void PlayerActive(bool isActive)
 		{
-			_currentUsePlayer.SetControlState(isActive
-				? PlayerManager.PlayerControlState.Normal
-				: PlayerManager.PlayerControlState.ForcedControl);
-			_currentUsePlayer.RPC_SetUseGrav(isActive);
-		}
-		private void OnInteractFixedNetworkUpdate(PlayerInput input)
-		{
-			CannonRotate(input.MoveDirection.x, input.LookDirection.y);
-			_currentUsePlayer.SetWarpTarget(_waitCharacterTransform.position, _waitCharacterTransform.rotation);
-
-			if (input.Buttons.IsSet(PlayerButtons.Attack) && LastFireTime.ExpiredOrNotRunning(Runner))
+			if (Runner.IsServer)
 			{
-				Debug.Log("Fire");
-				Fire().Forget();
-				LastFireTime = TickTimer.CreateFromSeconds(Runner, _reloadTime);
-				_currentAmmo -= 1;
-				if (_currentAmmo <= 0)
-				{
-					//終了処理
-					Debug.Log("終了");
-					IsActive = false;
-					OnInteractEnd();
-				}
+				_currentUsePlayer.RPC_SetControlState(isActive
+					? PlayerManager.PlayerControlState.Normal
+					: PlayerManager.PlayerControlState.ForcedControl);
+				_currentUsePlayer.RPC_SetUseGrav(isActive);
 			}
+		}
+
+		[Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+		private void RPC_PlayerUnActive()
+		{
+			PlayerActive(false);
+		}
+
+		[Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+		private void RPC_Fire()
+		{
+			_ = Fire();
 		}
 
 		private async UniTask Fire()
 		{
 			if (!Runner.IsServer) return;
 			var timer = 0f;
-			var effect = Runner.Spawn(_fireParticlePrefab, _muzzle.position, _muzzle.rotation);
 
 			// 発射向きなどの情報を保持しておく
 			var muzzlePosition = _muzzle.position;
@@ -196,6 +217,8 @@ namespace InGame.Exhibit
 			var endPos = _linePositions[_lastPositionIndex];
 			var endTime = _simulationStepTime * _lastPositionIndex;
 			
+			RPC_ParticleRenderActive(_fireParticle, true);
+
 			// 弾丸の位置をUpdateする
 			while (timer < endTime)
 			{
@@ -203,7 +226,7 @@ namespace InGame.Exhibit
 				var pos = TrajectoryCalculator(muzzlePosition,
 					muzzleForward * ballSpeed, 9.8f, timer);
 				//弾の位置更新
-				effect.transform.position = pos;
+				_fireParticle.transform.position = pos;
 
 				// シミュレーション終了条件
 				if ((endPos - pos).magnitude < 0.1f) // TODO:計算負荷
@@ -212,18 +235,18 @@ namespace InGame.Exhibit
 				await UniTask.WaitForSeconds(Runner.DeltaTime);
 			}
 
-			Destroy(effect.gameObject);
-			Runner.Despawn(effect);
+			RPC_ParticleRenderActive(_fireParticle, false);
 
 			// 着弾時のエフェクト
 			var explosionEffect = Runner.Spawn(_explosionParticlePrefab, endPos, Quaternion.identity);
 			Destroy(explosionEffect.gameObject, 3f);
 			Runner.Despawn(explosionEffect);
 
-			var cols = Physics.OverlapSphere(endPos, _radius, _playerHitMask);// TODO:当たり判定統一するかも
+			var cols = Physics.OverlapSphere(endPos, _radius, _playerHitMask); // TODO:当たり判定統一するかも
 			// ダメージ処理
 			foreach (var col in cols)
 			{
+				Debug.Log("Hit: " + col.name);
 				var damageable = col.GetComponentInParent<IDamageable>();
 				if (damageable == null) continue;
 				if (damageable.OwnerPlayerRef == _ownerPlayerRef) continue;
@@ -238,18 +261,34 @@ namespace InGame.Exhibit
 			var currentBaseAxis = _cannonBase.localEulerAngles.y +
 			                      _baseRotateSpeed * baseRotateInput;
 			currentBaseAxis = WarpAngle(currentBaseAxis);
-			_cannonBase.localRotation = _baseDefaultRotation * Quaternion.Euler(0,
+			_baseRotation = _baseDefaultRotation * Quaternion.Euler(0,
 				Mathf.Clamp(currentBaseAxis, _angleLimitY.x, _angleLimitY.y), 0);
 
 			// 砲身の回転
 			var currentBarrelAxis = _cannonBarrel.localEulerAngles.x +
 			                        _barrelRotateSpeed * barrelRotateInput;
 			currentBarrelAxis = WarpAngle(currentBarrelAxis);
-			_cannonBarrel.localRotation = _cannonBase.localRotation * Quaternion.Euler(
+			_barrelRotation = _cannonBase.localRotation * Quaternion.Euler(
 				Mathf.Clamp(currentBarrelAxis, _angleLimitX.x, _angleLimitX.y), 0, 0);
 		}
 		
+		[Rpc(RpcSources.All, RpcTargets.All)]
+		void RPC_ParticleRenderActive(NetworkObject obj, bool isActive)
+		{
+			obj.gameObject.SetActive(isActive);
+			// var particle = obj.GetComponentInChildren<ParticleSystem>();
+			// if (IsActive)
+			// {
+			// 	particle.Play();
+			// }
+			// else
+			// {
+			// 	particle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+			// }
+		}
+
 		#region Math Helper
+
 		/// <summary>
 		///     角度を-180~180に変換する
 		/// </summary>
@@ -269,6 +308,7 @@ namespace InGame.Exhibit
 		{
 			return startPos + power * time + Vector3.down * (gravity * time * time);
 		}
+
 		#endregion
 	}
 }
