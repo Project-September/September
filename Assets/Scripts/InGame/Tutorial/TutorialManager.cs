fac3cd68091419a45417ce48975cbda26f519381
KRYG <abcdef0509a@gmail.com>
2026-06-09 21:45:21 +0900

    [add]チュートリアル用の管理クラスの仮実装

---
+++ Assets/Scripts/InGame/Tutorial/TutorialManager.cs
@@ -0,0 +1,28 @@
+﻿using Fusion;
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace September.InGame.Tutorial
+{
+    public class TutorialManager : NetworkBehaviour
+    {
+        /// <summary>チュートリアルでプレイさせたいアクション配列</summary>
+        [SerializeReference, SubclassSelector] private List<TutorialActionBase> _tutorialActions;
+
+        public override void Spawned()
+        {
+            base.Spawned();
+        }
+
+        private void Start()
+        { 
+            _tutorialActions[0].OnStart();
+        }
+
+        private void Update()
+        {
+            _tutorialActions[0].OnUpdate();
+        }
+    }
+}
