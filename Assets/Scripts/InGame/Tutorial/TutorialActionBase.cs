c85bda3d42c1e5fd2ef15a60d81840521f024ead
KRYG <abcdef0509a@gmail.com>
2026-06-09 21:47:49 +0900

    [add]チュートリアル用で使用する基底クラスの実装

---
+++ Assets/Scripts/InGame/Tutorial/TutorialActionBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace September
+{
+    /// <summary>チュートリアルで使用する行動</summary>
+    [Serializable]
+    public class TutorialActionBase
+    {
+        public virtual void OnStart() { }
+        public virtual void OnUpdate() { }
+        public virtual void OnEndAction() { }
+    }
+}
