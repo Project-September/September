e3c50eb2540e3cce29d4155ed9716f7c265c2a2a
KRYG <abcdef0509a@gmail.com>
2026-06-16 19:27:04 +0900

    [update]namespaceの変更と移動のチュートリアルを複数の座標に対応させた

--- Assets/Scripts/InGame/Tutorial/TutorialActionBase.cs
+++ Assets/Scripts/InGame/Tutorial/TutorialActionBase.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using UnityEngine;
 
-namespace September
+namespace September.InGame.Tutorial
 {
     /// <summary>チュートリアルで使用する行動</summary>
     [Serializable]
     public class TutorialActionBase
     {
-        public virtual void OnStart() { }
+        public virtual void OnStart(Action action) { }
         public virtual void OnUpdate() { }
         public virtual void OnEndAction() { }
     }
--- Assets/Scripts/InGame/Tutorial/TutorialActionMove.cs
+++ Assets/Scripts/InGame/Tutorial/TutorialActionMove.cs
@@ -1,32 +1,53 @@
 ﻿using System;
 using UnityEngine;
 
-namespace September
+namespace September.InGame.Tutorial
 {
     [Serializable]
     public class TutorialActionMove : TutorialActionBase
     {
         [SerializeField] private Transform[] _moveTarget;
-        [SerializeField] private GameObject _targetObj;
+        [SerializeField] private GameObject _targetDisplayObj;
         [SerializeField] private float _targetRange = 3f;
         [SerializeField] private LayerMask _playerLayer = 1 << 6; // プレイヤーのレイヤー
         private int _index = 0;
         private const int CHECK_FRAME_INTERVAL = 5;
-        public override void OnStart()
+        private Action _onCompleted;
+        public override void OnStart(Action action)
         {
-            base.OnStart();
-            Debug.Log(_targetObj.transform.position);
-            _targetObj.transform.position = _moveTarget[_index].position;
-            Debug.Log(_targetObj.transform.position);
+            base.OnStart(action);
+            _onCompleted = action;
+            Debug.Log(_targetDisplayObj.transform.position);
+            _targetDisplayObj.transform.position = _moveTarget[_index].position;
+            Debug.Log(_targetDisplayObj.transform.position);
         }
 
         public override void OnUpdate()
         {
             base.OnUpdate();
+            // プレイヤーがターゲットの範囲内にいるかどうかをチェック
             if (IsInPlayerInTarget())
             {
-                Debug.Log("範囲内に入ったよ");
+                ToNextTarget();
             }
+        }
+
+        /// <summary>
+        /// 次のターゲットに移動する処理
+        /// </summary>
+        private void ToNextTarget()
+        {
+            _index++;
+            // ターゲットのインデックスが範囲外になった場合の処理
+            if (_index >= _moveTarget.Length)
+            {
+                // すべてのターゲットをクリアした場合の処理
+                Debug.Log("すべてのターゲットをクリアしました！");
+                _onCompleted?.Invoke();
+                return;
+            }
+            // 次のターゲットに移動
+            _targetDisplayObj.transform.position = _moveTarget[_index].position;
         }
 
         /// <summary>
@@ -35,9 +56,20 @@ namespace September
         private bool IsInPlayerInTarget()
         {
             // 処理の負荷を減らすため、毎フレームチェックするのではなく、5フレームに1回チェックする
-            if (Time.frameCount % CHECK_FRAME_INTERVAL != 0)  return false;
+            if (Time.frameCount % CHECK_FRAME_INTERVAL != 0) return false;
+
+            if (_index >= _moveTarget.Length)
+            {
+                Debug.LogWarning("ターゲットのインデックスが範囲外です。");
+                return false;
+            }
+
             // 指定した範囲内にプレイヤーがいるかどうかをチェック
-            Collider[] hitColliders = Physics.OverlapSphere(_moveTarget[_index].position, _targetRange, _playerLayer);         
+            Collider[] hitColliders = Physics.OverlapSphere(
+                _moveTarget[_index].position,
+                _targetRange,
+                _playerLayer);
+
             // 範囲内にプレイヤーがいる場合はtrueを返す
             if (hitColliders.Length > 0)
             {
@@ -49,6 +81,8 @@ namespace September
         public override void OnEndAction()
         {
             base.OnEndAction();
+            _onCompleted = null;
+            _index = 0;
         }
     }
 }
