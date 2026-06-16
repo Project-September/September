e801c1306026ba6c2b4ca3ede254052946302608
KRYG <abcdef0509a@gmail.com>
2026-06-16 19:31:29 +0900

    [update] 実行中のチュートリアル終了時に次のチュートリアルに移行できるようにした

--- Assets/Scripts/InGame/Tutorial/TutorialManager.cs
+++ Assets/Scripts/InGame/Tutorial/TutorialManager.cs
@@ -1,5 +1,5 @@
 ﻿using Fusion;
-using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +9,9 @@ namespace September.InGame.Tutorial
     {
         /// <summary>チュートリアルでプレイさせたいアクション配列</summary>
         [SerializeReference, SubclassSelector] private List<TutorialActionBase> _tutorialActions;
+        private int _currentActionIndex = 0;
+
+        private bool _isTutorialCompleted = false;
 
         public override void Spawned()
         {
@@ -16,13 +19,42 @@ namespace September.InGame.Tutorial
         }
 
         private void Start()
-        { 
-            _tutorialActions[0].OnStart();
+        {
+            // 仮のチュートリアル開始(仕様が決まり次第、適切なタイミングで呼び出す)
+            OnTutorialStart();
+        }
+
+        /// <summary>
+        /// チュートリアルを開始するメソッド
+        /// 最初のアクションを開始し、以降は各アクションの完了コールバックで次のアクションを開始する
+        /// </summary>
+        public void OnTutorialStart()
+        {
+            _tutorialActions[_currentActionIndex].OnStart(OnCompleteCurrentAction);
         }
 
         private void Update()
         {
-            _tutorialActions[0].OnUpdate();
+            if (_isTutorialCompleted) return;
+            _tutorialActions[_currentActionIndex].OnUpdate();
+        }
+
+        private void OnCompleteCurrentAction()
+        {
+            // 現在のアクションを終了する
+            _tutorialActions[_currentActionIndex].OnEndAction();
+            _currentActionIndex++;
+            // 次のアクションがあれば開始する
+            if (_currentActionIndex < _tutorialActions.Count)
+            {
+                _tutorialActions[_currentActionIndex].OnStart(OnCompleteCurrentAction);
+            }
+            // すべてのアクションが完了した場合の処理
+            else
+            {
+                Debug.Log("チュートリアルが完了しました！");
+                _isTutorialCompleted = true;
+            }
         }
     }
 }
