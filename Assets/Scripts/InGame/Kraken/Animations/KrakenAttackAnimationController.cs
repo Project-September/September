6f871ff978f93dbff4dc7c2cb6b73169bd02b5be
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-07-04 14:57:11 +0900

    fix: 触手向きを変えられない問題修正
    もともとIKの影響下にないオブジェクトを回転させていたところを、IKの構成を変えたために回転がIKによって常に上書きされてしまっていた

--- Assets/Scripts/InGame/Kraken/Animations/KrakenAttackAnimationController.cs
+++ Assets/Scripts/InGame/Kraken/Animations/KrakenAttackAnimationController.cs
@@ -5,42 +5,39 @@ using UnityEngine;
 
 namespace September.InGame.Kraken.Animations
 {
-    // Todo: 触手の向きを変えられない問題修正
     public class KrakenAttackAnimationHandler : MonoBehaviour
     {
         [Serializable]
         private class KrakenTentacleAnimationSettings
         {
             [SerializeField] private Animator _animator;
+            [SerializeField] private Transform _armRoot;
             [SerializeField] private FABRIKRoot _fabrikRoot;
             [SerializeField] private string _animationName;
 
             private Quaternion _startRotation;
-            private Quaternion _lookRotation;
 
             public void LookAt(Vector3 target)
             {
-                _startRotation = _fabrikRoot.transform.rotation;
+                Transform root = _armRoot;
 
-                var forward = -_fabrikRoot.transform.right;
-                var dir = Vector3.ProjectOnPlane(target - _fabrikRoot.transform.position, _fabrikRoot.transform.up).normalized;
-                Debug.DrawRay(_fabrikRoot.transform.position, dir * 100f, Color.red, 3f);
+                _startRotation = root.rotation;
+
+                var forward = -root.transform.right;
+                var dir = Vector3.ProjectOnPlane(target - root.position, root.up).normalized;
+                Debug.DrawRay(root.position, dir * 100f, Color.red, 3f);
 
                 var rot = Quaternion.FromToRotation(forward, dir);
                 Debug.Log($"{target} {forward} {dir} {rot.eulerAngles}");
+
+                root.rotation *= rot;
             }
 
             public async UniTask PlayAnimation()
             {
-                IKSolver.UpdateDelegate del = () =>
-                {
-                    _fabrikRoot.transform.rotation *= _lookRotation;
-                };
-
-                _fabrikRoot.solver.OnPreUpdate += del;
                 _animator.Play(_animationName, 0, 0f);
                 await _animator.WaitUntilEndState(_animationName);
-                _fabrikRoot.solver.OnPostUpdate -= del;
+                _armRoot.transform.rotation = _startRotation;
             }
         }
 
