78364835ad7cd1d4a77756282798c2c5e76ff7b8
KRYG <abcdef0509a@gmail.com>
2026-07-12 01:56:18 +0900

    [add]チュートリアル用の基底クラス

---
+++ Assets/Scripts/InGame/Tutorial/TutorialActions/TutorialActionBase.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using InGame.Player;    
+
+namespace September.InGame.Tutorial
+{
+    /// <summary>
+    /// チュートリアルで使用する行動のデータ
+    /// </summary>
+    public struct TutorialActionData
+    {
+        public Action Action;
+        public GameObject Player;
+        public Image Image;
+        public PlayerInputManager PlayerInputManager;
+    }
+    /// <summary>チュートリアルで使用する行動</summary>
+    [Serializable]
+    public class TutorialActionBase
+    {
+        [SerializeField] protected Sprite _explanationPicture;
+        protected bool _isCompleted = false;
+        protected TutorialActionData _actionData;
+        protected bool _isActionStarted = false;
+        public virtual void OnStart(TutorialActionData actionData)
+        {
+            _actionData = actionData;
+            actionData.Image.enabled = true;
+            actionData.Image.sprite = _explanationPicture;
+            _isActionStarted = true;
+        }
+
+        public virtual void OnUpdate()
+        {
+            if (!_isActionStarted) return;
+        }
+        public virtual void OnEndAction() { }
+    }
+}
