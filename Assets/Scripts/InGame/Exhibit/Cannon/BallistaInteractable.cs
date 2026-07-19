a4cf42d5286bf8894b7586bcc9e7b5484122fdaa
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2026-07-19 18:27:24 +0900

    fix:移動処理とリロード処理のテスト
    この処理について、Projectile部分に対しての変更が必要なpushになっているため、プルリク用に削除しますが、戻ってこれるようにするためのlogです

--- Assets/Scripts/InGame/Exhibit/Cannon/BallistaInteractable.cs
+++ Assets/Scripts/InGame/Exhibit/Cannon/BallistaInteractable.cs
@@ -1,7 +1,36 @@
+using Fusion;
+using September.Common;
+using UnityEngine;
+
 namespace September.InGame.Exhibit
 {
     public class BallistaInteractable: ProjectileInteractableBase
     {
-        
+        [SerializeField] private float _reloadSeconds = 10f;
+        [Networked] private TickTimer ReloadTimer { get; set; }
+
+        protected override void OnInteractFixedUpdate()
+        {
+            if (ReloadTimer.Expired(Runner))
+            {
+                ReloadTimer = default(TickTimer);
+                _currentAmmo = _maxAmmo;
+                Debug.Log("reload");
+            }
+        }
+
+        protected override void Fire()
+        {
+            if(!ReloadTimer.ExpiredOrNotRunning(Runner)) return;
+            _launcher.Fire(CurrentUsePlayerRef);
+            _currentAmmo -= 1;
+            LastFireTimer = TickTimer.CreateFromSeconds(Runner, _reloadTime);
+            
+            if (_currentAmmo <= 0)
+            {
+                Debug.Log("current ammo is 0");
+                ReloadTimer = TickTimer.CreateFromSeconds(Runner, _reloadSeconds);
+            }
+        }
     }
 }
--- Assets/Scripts/InGame/Exhibit/Cannon/BallistaMove.cs
+++ Assets/Scripts/InGame/Exhibit/Cannon/BallistaMove.cs
@@ -1,7 +1,4 @@
-using System;
-using Common.UserSettings;
 using Fusion;
-using InGame.Player;
 using September.Common;
 using UnityEngine;
 
@@ -13,15 +10,15 @@ namespace September.InGame.Exhibit
 		[SerializeField] private Transform _rotateBase;
 		[SerializeField] private BallistaCamera _cameraController;
 		[SerializeField] private LayerMask _layerMask;
-		[SerializeField] private Vector2 _pitchMinMax;
-		[SerializeField] private Vector2 _yawMinMax;
 		[SerializeField] private float _sens;
 		[SerializeField] private float _padSens;
+		[SerializeField] private float _playerOffset = 3;
 
+		private NetworkObject _playerObject;
 		private float _basePitch;
 		private float _baseYaw;
-		private float _pitch;
-		private float _yaw;
+		[Networked] private float Pitch { get; set; }
+		[Networked] private float Yaw { get; set; }
 
 		public override void Spawned()
 		{
@@ -30,41 +27,48 @@ namespace September.InGame.Exhibit
 			_cameraController.Initialize();
 		}
 
+		public override void Render()
+		{
+			_rotateBase.transform.rotation = Quaternion.Euler(0, Yaw + _baseYaw, 0);
+			_barrel.rotation = Quaternion.Euler(_basePitch + Pitch, Yaw + _baseYaw, 0);
+		}
+
+		public void Initialize(NetworkObject playerObject, PlayerRef playerRef)
+		{
+			_playerObject = playerObject;
+		}
+
 		public void MoveUpdate(PlayerInput input)
 		{
 			var moveInput = input.LookDirection;
-			RotateCamera(moveInput, Runner.DeltaTime);
+			_cameraController.RotateCamera(moveInput, Runner.DeltaTime);
 
-			var cameraForward = _cameraController.transform.forward.normalized;
-			if (Physics.Raycast(_cameraController.transform.position, cameraForward, out var hit, 100, _layerMask))
+			var cameraTf = _cameraController.GetCameraTf();
+			if (Physics.Raycast(cameraTf.position, cameraTf.forward, out var hit, 100, _layerMask))
 			{
 				var direction = hit.point - _rotateBase.transform.position;
-				var euler = Quaternion.LookRotation(direction).eulerAngles;
-				_pitch = Mathf.Clamp(euler.x, _pitchMinMax.x, _pitchMinMax.y);
-				_yaw = Mathf.Clamp(euler.y, _yawMinMax.x, _yawMinMax.y);
+				var euler = Quaternion.LookRotation(direction.normalized).eulerAngles;
+				Debug.Log(euler);
+				Pitch = euler.x;
+				Yaw = euler.y;
+				
+				Debug.Log("pitch" + Pitch + " yaw" + Yaw);
 			}
-
-			_rotateBase.transform.rotation = Quaternion.Euler(0, _yaw + _baseYaw, 0);
-			_barrel.rotation = Quaternion.Euler(_basePitch + _pitch, _yaw + _baseYaw, 0);
-		}		
-		
-		public void RotateCamera(Vector2 input, float deltaTime)
-		{
-			var settings = UserSettings.Get();
-			var sens =
-				GameInput.I.UseDeviceType == GameInput.DeviceType.KeyboardMouse
-					? _sens * settings.MouseSensitivity
-					: _padSens * settings.PadSensitivity;
+			else
+			{
+				Pitch = _cameraController.GetCameraTf().eulerAngles.x;
+				Yaw = _cameraController.GetCameraTf().eulerAngles.y;
+			}
 			
-			float pitchInput = input.y;
-			float yawInput = input.x;
-
-			_pitch -= pitchInput * deltaTime * sens;
-			_pitch = Mathf.Clamp(_pitch, _pitchMinMax.x - _basePitch, _basePitch - _pitchMinMax.y);
-			_yaw -= yawInput * deltaTime * sens;
-			_yaw = Mathf.Clamp(_yaw, _yawMinMax.x - _baseYaw, _baseYaw - _yawMinMax.y);
+			UpdatePlayerPosition();
 		}
 
+		void UpdatePlayerPosition()
+		{
+			var quaternion = Quaternion.Euler(0, Yaw, 0);
+			_playerObject.transform.rotation = quaternion;
+			_playerObject.transform.position = _rotateBase.position + quaternion * (Vector3.back * _playerOffset);
+		}
 		public void Refresh()
 		{ 
 			_cameraController.ResetCamera();
