f124473c487ad2a1b209a9f026d0e36b0c5a2ed3
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-07-20 03:36:16 +0900

    fix: UnityEditor依存箇所の修正

--- Assets/Scripts/Common/MapSceneContainer.cs
+++ Assets/Scripts/Common/MapSceneContainer.cs
@@ -3,8 +3,11 @@ using System.Linq;
 using Cysharp.Threading.Tasks;
 using InGame.Common;
 using NaughtyAttributes;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace September.Common
 {
