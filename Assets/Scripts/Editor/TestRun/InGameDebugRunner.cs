a6b4ad835359f6181281e0345d76068e0d3b5dd1
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-07-20 02:24:02 +0900

    feat: InGameDebugWindowにマップ選択機能追加

--- Assets/Scripts/Editor/TestRun/InGameDebugRunner.cs
+++ Assets/Scripts/Editor/TestRun/InGameDebugRunner.cs
@@ -96,7 +96,7 @@ namespace September.Editor.InGameDebug
 				if (index < PlayersData.Count)
 					playerDatabase.Rpc_SetCharacter(player.Key, PlayersData[index++].CharacterType);
 
-			networkManager.StartGame(new GameStartContext(MapType.Museum)).Forget();
+			networkManager.StartGame(new GameStartContext(_lobbyData.MapType)).Forget();
 		}
 
 		private async UniTask JoinLobby()
