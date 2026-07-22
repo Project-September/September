2ab97deaa487574f34ca09461578128e1e41739c
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-07-23 03:47:04 +0900

    feat: ナビゲーション先を追加・変更

--- Assets/Scripts/Editor/SceneNavigation.cs
+++ Assets/Scripts/Editor/SceneNavigation.cs
@@ -13,19 +13,26 @@ public static class SceneNavigation
         OpenScene(0);
     }
 
-    [MenuItem("Scene/GameScene")]
-    public static void Scene01()
+    [MenuItem("Scene/Map_Museum")]
+    public static void Museum()
     {
         EditorSceneManager.SaveOpenScenes();
         OpenScene(1);
         OpenScene(5, OpenSceneMode.Additive);
     }
 
-    [MenuItem("Scene/ResultScene")]
-    public static void Scene02()
+    [MenuItem("Scene/Map_Pirate")]
+    public static void Pirate()
     {
         EditorSceneManager.SaveOpenScenes();
-        OpenScene(2);
+        OpenScene(9);
+    }
+
+    [MenuItem("Scene/ResultScene")]
+    public static void Result()
+    {
+        EditorSceneManager.SaveOpenScenes();
+        OpenScene(7);
     }
 
     [MenuItem("Scene/DevStartInScene")]
