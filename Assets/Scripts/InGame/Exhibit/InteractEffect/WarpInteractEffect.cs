af9cef898a682936c9a498ae32f4db9fc752de5a
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2026-07-25 01:20:14 +0900

    fix:warp展示物でマテリアルが消える不具合対応、飛行機の操作説明を追加

--- Assets/Scripts/InGame/Exhibit/InteractEffect/WarpInteractEffect.cs
+++ Assets/Scripts/InGame/Exhibit/InteractEffect/WarpInteractEffect.cs
@@ -82,17 +82,21 @@ namespace InGame.Exhibit
             FormationManager formationManager = player.GetComponent<FormationManager>();
             formationManager?.WarpFriendNearPlayer(targetPos, targetRot);
 
-            // 少し待ってから移動予約
-            await UniTask.Delay(TimeSpan.FromSeconds(_warpDuration), cancellationToken: _cts.Token);
-
-            // ゴール側エフェクトの再生
-            PlayEffect(EffectType.WarpOut, targetPos, Quaternion.identity);
-            SetPlayerVisible(player, true);
-            _interactableBase.ForceSetInteractable = true;
-
-            if (_audioBroadcaster != null)
+            try
             {
-                _audioBroadcaster.RPC_PlaySoundFromCode(_warpOutSoundName, SoundTrackingType.Spot, player.Id, default); // 発音元を一旦Playerに
+                // 少し待ってから移動予約
+                await UniTask.Delay(TimeSpan.FromSeconds(_warpDuration), cancellationToken: _cts.Token);
+            }
+            finally
+            {
+                PlayEffect(EffectType.WarpOut, targetPos, Quaternion.identity);
+                SetPlayerVisible(player, true);
+                _interactableBase.ForceSetInteractable = true;
+
+                if (_audioBroadcaster != null)
+                {
+                    _audioBroadcaster.RPC_PlaySoundFromCode(_warpOutSoundName, SoundTrackingType.Spot, player.Id, default); // 発音元を一旦Playerに
+                }
             }
         }
 
