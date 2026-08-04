c55264113a11943d9a1044551f4230fc61de8d13
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-08-04 22:47:12 +0900

    feat: クラーケン退場中にインタラクトできないように

--- Assets/Scripts/InGame/Kraken/Kraken.cs
+++ Assets/Scripts/InGame/Kraken/Kraken.cs
@@ -213,6 +213,7 @@ namespace September.InGame.Kraken
         private async UniTaskVoid Disappear()
         {
             _appearanceState = KrakenAppearanceState.Disappear;
+            _interactable.gameObject.SetActive(false);
             await _playableDirector.PlayAsync(_outTimeline);
             if (HasStateAuthority) Runner.Despawn(Object);
         }
