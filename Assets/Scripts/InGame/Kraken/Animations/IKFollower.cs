f83f90eb44789337aeed02a9e880dde06e41e1c3
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-08-04 20:28:02 +0900

    refactor: IKFollowerの処理を削減。一部命名の修正
    ListだとSpanとして直接渡せないので配列に変更し、Spanに詰め替えるだけの処理をしないで済むようにした

--- Assets/Scripts/InGame/Kraken/Animations/IKFollower.cs
+++ Assets/Scripts/InGame/Kraken/Animations/IKFollower.cs
@@ -59,7 +59,7 @@ namespace September.InGame.Kraken
         [SerializeField] private TentacleConstraintSolver _constraintSolver;
         [SerializeField] private int _subPointCount = 3;
 
-        [SerializeField, HideInInspector] private List<float> _maxDistanceList = new();
+        [SerializeField, HideInInspector] private float[] _maxDistances;
         [SerializeField, HideInInspector] private Quaternion[] _defaultRotations;
 
         [Header("Debug Settings")]
@@ -77,17 +77,15 @@ namespace September.InGame.Kraken
         private void Start()
         {
             // ボーンの長さをキャッシュ
-            _maxDistanceList.Add(0);
+            _maxDistances = new float[_followers.Length];
             for (int i = 1; i < _followers.Length; i++)
             {
                 var p0 = _followers[i - 1].position;
                 var p1 = _followers[i].position;
 
-                _maxDistanceList.Add((p1 - p0).magnitude);
+                _maxDistances[i] += (p1 - p0).magnitude + _maxDistances[i - 1];
 
-                _maxDistanceList[i] += _maxDistanceList[i - 1];
-
-                Debug.Log($"p0:{p0} p1:{p1} distance:{(p1 - p0).magnitude} sum:{_maxDistanceList[i]}");
+                Debug.Log($"p0:{p0} p1:{p1} distance:{(p1 - p0).magnitude} sum:{_maxDistances[i]}");
             }
 
             // ボーンの回転をキャッシュ
@@ -100,9 +98,9 @@ namespace September.InGame.Kraken
 
         private void Validate()
         {
-            Debug.Assert(_maxDistanceList.Count > 0, "Max distance list is empty");
-            Debug.Assert(_maxDistanceList.Skip(1).All(x => x > float.Epsilon), "Max distance element is too small");
-            Debug.Assert(_maxDistanceList.SequenceEqual(_maxDistanceList.OrderBy(x => x)), $"Max distance is not ordered\n{string.Join(",\n", _maxDistanceList)}");
+            Debug.Assert(_maxDistances.Length > 0, "Max distance list is empty");
+            Debug.Assert(_maxDistances.Skip(1).All(x => x > float.Epsilon), "Max distance element is too small");
+            Debug.Assert(_maxDistances.SequenceEqual(_maxDistances.OrderBy(x => x)), $"Max distance is not ordered\n{string.Join(",\n", _maxDistances)}");
             Debug.Assert(_defaultRotations.Length > 0, "Default rotation array is empty");
         }
 
@@ -110,21 +108,16 @@ namespace September.InGame.Kraken
         private void Update()
         {
             Profiler.BeginSample("GetPoints");
-            // FABRIKで障害物がない時のボーン位置を計算
+            // FABRIKで障害物がない時のボーン位置を取得
             Point[] points = _pointProvider.GetPoints();
             Profiler.EndSample();
 
             IKFollowerDebug.DebugDraw(points.ToArray(), _radius * .2f, Color.yellow, _debugFabrikPoints, _debugFabrikPoints, _debugFabrikAxis);
 
             Profiler.BeginSample("EvaluateSpline SubPoints");
-            Span<float> maxDistanceList = stackalloc float[_maxDistanceList.Count];
-            for (int i = 0; i < _maxDistanceList.Count; i++)
-            {
-                maxDistanceList[i] = _maxDistanceList[i];
-            }
-            SlerpInterpolator spline = new(points, maxDistanceList);
+            SlerpInterpolator interpolator = new(points, _maxDistances);
             Span<Point> subdividedPoints = stackalloc Point[points.Length * _subPointCount];
-            spline.Evaluate(points.Length * _subPointCount, ref subdividedPoints);
+            interpolator.Evaluate(points.Length * _subPointCount, ref subdividedPoints);
             Profiler.EndSample();
 
             Profiler.BeginSample("SolveCollision");
@@ -142,9 +135,9 @@ namespace September.InGame.Kraken
                     solvedDistances[i - 1] +
                     (solvedPoints[i].Position - solvedPoints[i - 1].Position).magnitude;
             }
-            SlerpInterpolator solvedSpline = new(solvedPoints, solvedDistances);
-            Span<Point> solvedResamplingPoints = stackalloc Point[_maxDistanceList.Count];
-            solvedSpline.Evaluate(_maxDistanceList, ref solvedResamplingPoints);
+            SlerpInterpolator solvedInterpolator = new(solvedPoints, solvedDistances);
+            Span<Point> solvedResamplingPoints = stackalloc Point[_maxDistances.Length];
+            solvedInterpolator.Evaluate(_maxDistances, ref solvedResamplingPoints);
             Profiler.EndSample();
 
             IKFollowerDebug.DebugDraw(solvedResamplingPoints.ToArray(), 6f, Color.magenta, _debugSolvedResamplingPoints, _debugSolvedResamplingPoints, _debugSolvedResamplingAxis);
