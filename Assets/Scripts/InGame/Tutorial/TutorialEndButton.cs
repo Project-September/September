9662d318de2b1126f746017cbead32e5b7312c43
KRYG <abcdef0509a@gmail.com>
2026-08-10 21:34:23 +0900

    [add]タイトルに戻る処理

---
+++ Assets/Scripts/InGame/Tutorial/TutorialEndButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using September.Common;
+
+namespace September
+{
+    public class TutorialEndButton : MonoBehaviour
+    {
+        [SerializeField] private Button _tutorialButton;
+
+        private void Start()
+        {
+            // 既存のボタン登録に追加
+            _tutorialButton.onClick.AddListener(() =>
+                NetworkManager.Instance.QuitLobby().Forget());
+        }
+    }
+}
