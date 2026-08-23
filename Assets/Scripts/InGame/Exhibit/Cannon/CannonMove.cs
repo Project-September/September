a69dae8b783da7bd0edfae4a7c66f5b4905c178a
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2026-08-24 00:28:09 +0900

    fix:大砲のモデル適応と、PlayerAnimation適応

--- Assets/Scripts/InGame/Exhibit/Cannon/CannonMove.cs
+++ Assets/Scripts/InGame/Exhibit/Cannon/CannonMove.cs
@@ -8,12 +8,14 @@ namespace September.InGame.Exhibit
 	{
 		[Header("移動関連")] [SerializeField] private Transform _cannonBase;
 		[SerializeField] private Transform _cannonBarrel;
+		[SerializeField] private Transform _waitPlayerPos;
 		[SerializeField] private float _baseRotateSpeed;
 		[SerializeField] private float _barrelRotateSpeed;
+
+		[Header("射撃角度制限")] [SerializeField] private bool _useYawAngleLimit = true;
 		// minをxとして、maxをyとして扱う
 		[SerializeField] private Vector2 _baseRotateAngleLimit = new(-90, 90);
 		[SerializeField] private Vector2 _barrelRotateAngleLimit = new(-90, 90);
-		[SerializeField] private float _playerOffset = 3f;
 		private Quaternion _baseDefaultRotation;
 		private Quaternion _barrelDefaultRotation;
 		[Networked] private NetworkObject Player { get; set; }
@@ -21,10 +23,11 @@ namespace September.InGame.Exhibit
 		[Networked] private Quaternion BarrelRotation { get; set; }
 
 		public override void Render()
-		{ 
+		{
 			base.Render();
 			_cannonBase.localRotation = BaseRotation;
 			_cannonBarrel.localRotation = BarrelRotation;
+			UpdatePlayerPos();
 		}
 
 		public void InitializeStateAuthority(NetworkObject playerObject, PlayerRef playerRef)
@@ -34,21 +37,21 @@ namespace September.InGame.Exhibit
 
 		public void Initialize()
 		{
-			
 		}
 
 		void IProjectileMovement.Update(PlayerInput input)
 		{
-			if(!HasStateAuthority) return;
+			if (!HasStateAuthority) return;
 			CannonRotate(input.MoveDirection.x, input.LookDirection.y);
 			UpdatePlayerPos();
 		}
 
 		public void Reset()
 		{
+			Player = null;
 			RPC_Refresh();
 		}
-		
+
 		[Rpc(RpcSources.All, RpcTargets.All)]
 		public void RPC_Refresh()
 		{
@@ -58,31 +61,33 @@ namespace September.InGame.Exhibit
 
 		private void UpdatePlayerPos()
 		{
-			var quaternion = BaseRotation;
-			Player.transform.rotation = quaternion;
-			Player.transform.position = _cannonBase.position + quaternion * (Vector3.back * _playerOffset);
+			if(!Player) return;
+			Player.transform.rotation = _waitPlayerPos.rotation;
+			Player.transform.position = _waitPlayerPos.position;
 		}
 
 		private void CannonRotate(float baseRotateInput, float barrelRotateInput)
 		{
 			baseRotateInput = Mathf.Clamp(baseRotateInput, -1, 1);
 			barrelRotateInput = Mathf.Clamp(barrelRotateInput, -1, 1);
-			
+
 			// 土台の回転
 			var currentBaseAxis = _cannonBase.localEulerAngles.y +
 			                      _baseRotateSpeed * baseRotateInput;
 			currentBaseAxis = WrapAngle(currentBaseAxis);
 			BaseRotation = Quaternion.Euler(0,
-				Mathf.Clamp(currentBaseAxis, _baseRotateAngleLimit.x, _baseRotateAngleLimit.y), 0);
+				_useYawAngleLimit
+					? Mathf.Clamp(currentBaseAxis, _baseRotateAngleLimit.x, _baseRotateAngleLimit.y)
+					: currentBaseAxis, 0);
 
 			// 砲身の回転
 			var currentBarrelAxis = _cannonBarrel.localEulerAngles.x +
 			                        _barrelRotateSpeed * barrelRotateInput;
 			currentBarrelAxis = WrapAngle(currentBarrelAxis);
-			BarrelRotation = _cannonBase.localRotation * Quaternion.Euler(
+			BarrelRotation = Quaternion.Euler(
 				Mathf.Clamp(currentBarrelAxis, _barrelRotateAngleLimit.x, _barrelRotateAngleLimit.y), 0, 0);
 		}
-		
+
 		/// <summary>
 		///     角度を-180~180に変換する
 		/// </summary>
