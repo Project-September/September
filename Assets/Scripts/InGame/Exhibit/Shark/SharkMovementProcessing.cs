3315298b4789b21a280541f461069dedd7df2d14
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-08-25 01:01:01 +0900

    refactor: 内容が古いコメントを更新
    地面判定を変更したため意味が通らなくなったコメントを修正

--- Assets/Scripts/InGame/Exhibit/Shark/SharkMovementProcessing.cs
+++ Assets/Scripts/InGame/Exhibit/Shark/SharkMovementProcessing.cs
@@ -102,7 +102,7 @@ public class SharkMovementProcessing : NetworkBehaviour
         if (moveDirection == Vector3.zero) return;
 
         var moveVelocity = Vector3.ProjectOnPlane(moveDirection, _currentGroundNormal);　//坂に沿った動きに
-        if (moveVelocity.y < 0) moveVelocity.y = 0; // 下りの場合は無視（頭側で判定しているので上りとして扱う、下りが必要な場合は尾側で判定する）
+        if (moveVelocity.y < 0) moveVelocity.y = 0; // 下りの場合は無視
         moveVelocity = moveVelocity.normalized;
 
         Debug.DrawRay(transform.position, _currentGroundNormal * 2, Color.yellow);
