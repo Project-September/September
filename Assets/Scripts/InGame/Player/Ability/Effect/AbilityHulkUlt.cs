6a29f9ffd13fab42c38fc6026ea3f4c1619d2f3f
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-08-28 21:04:29 +0900

    fix: ExhibitRegistryが存在しない場合にハルク必殺技が発動しない問題を修正

--- Assets/Scripts/InGame/Player/Ability/Effect/AbilityHulkUlt.cs
+++ Assets/Scripts/InGame/Player/Ability/Effect/AbilityHulkUlt.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using InGame.Exhibit;
+using InGame.Interact;
 using September.Common;
 using September.InGame.Effect;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace InGame.Player.Ability.Effect
 {
@@ -18,6 +21,7 @@ namespace InGame.Player.Ability.Effect
         [SerializeField] private Vector3 _effectOffset;
         
         private EffectSpawner _effectSpawner;
+        private IReadOnlyList<InteractableBase> _exhibits;
 
         protected override void OnCutInEnd()
         {
@@ -30,8 +34,9 @@ namespace InGame.Player.Ability.Effect
         {
             var player = Parameter.Owner;
 
-            var items = ExhibitRegistry.I.Items;
-            foreach (var item in items)
+            _exhibits ??= GetExhibits();
+
+            foreach (var item in _exhibits)
             {
                 var sqDistance = (item.transform.position - player.transform.position).sqrMagnitude;
                 if (sqDistance > _radius * _radius) continue;
@@ -41,6 +46,19 @@ namespace InGame.Player.Ability.Effect
 
             _effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
             _effectSpawner?.RequestPlayOneShotEffect(_effectType, player.transform.position + player.transform.rotation * _effectOffset, Quaternion.identity);
+        }
+
+        private IReadOnlyList<InteractableBase> GetExhibits()
+        {
+            if (ExhibitRegistry.I != null)
+            {
+                return ExhibitRegistry.I.Items;
+            }
+            else
+            {
+                Debug.LogWarning("[AbilityHulkUlt] ExhibitRegistry が存在しないため、全探索を行います");
+                return Object.FindObjectsByType<InteractableBase>(FindObjectsSortMode.None);
+            }
         }
     }
 }
