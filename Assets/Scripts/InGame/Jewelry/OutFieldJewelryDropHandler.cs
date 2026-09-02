b7c9e81dd929b1a95e4f7f0516f4f24226ea6290
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-09-03 03:28:28 +0900

    feat: 到達時間をランダム化

--- Assets/Scripts/InGame/Jewelry/OutFieldJewelryDropHandler.cs
+++ Assets/Scripts/InGame/Jewelry/OutFieldJewelryDropHandler.cs
@@ -16,6 +16,7 @@ namespace September.InGame.Jewelry
 
         [SerializeField] private float _height = 1f;
         [SerializeField] private float _duration = 0.7f;
+        [SerializeField] private float _randomDuration = 0.1f;
         [SerializeField] private AnimationCurve _ease;
 
         private IJewelryContainer _jewelryContainer;
@@ -50,7 +51,7 @@ namespace September.InGame.Jewelry
                 {
                     Vector3 pos = GetRandomPosition();
                     DebugDrawUtility.DrawWireSphere(pos, 1f, Color.red, 5f);
-                    jewelry.JewelryControl.ThrowToNonPhysics(pos, (pos.y - transform.position.y) + _height, _duration, _ease);
+                    jewelry.JewelryControl.ThrowToNonPhysics(pos, (pos.y - transform.position.y) + _height, _duration + Random.Range(-.5f, .5f) * _randomDuration, _ease);
                 }
             }
         }
