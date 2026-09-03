7497a224f27956f445565d6f210fb99f1b4b645f
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-09-04 00:15:56 +0900

    feat: 宝石ドロップ設定を適用。直前に攻撃を与えたプレイヤーが自動取得する処理を追加。

--- Assets/Scripts/InGame/Jewelry/OutFieldJewelryDropHandler.cs
+++ Assets/Scripts/InGame/Jewelry/OutFieldJewelryDropHandler.cs
@@ -1,17 +1,30 @@
+using Cysharp.Threading.Tasks;
+using Fusion;
+using InGame.Health;
 using InGame.Jewelry.Common;
 using InGame.Player;
+using NaughtyAttributes;
 using September.Common.Attribute;
+using September.InGame.Jewelry.Drop;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 namespace September.InGame.Jewelry
 {
-    public class OutFieldJewelryDropHandler : MonoBehaviour
+    [SimulationBehaviour(Modes = SimulationModes.Host | SimulationModes.Server)]
+    public class OutFieldJewelryDropHandler : NetworkBehaviour
     {
         [SerializeField] private PlayerRespawn _playerRespawn;
         [SerializeField, RequireInterface(typeof(IJewelryContainer))] private Component _jewelryContainerObj;
         [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private PlayerHealth _playerHealth;
+
+        [Header("宝石ドロップ設定")]
+        [InfoBox("直前に攻撃を受けてから落下した場合、その攻撃情報をもとに宝石ドロップを処理します。")]
+        [SerializeField] private JewelryDropSettingsContainer[] _dropSettings;
+        [SerializeField, Tooltip("直前に受けた攻撃情報の保持秒数")]
+        private float _hitRetentionTime = 20f;
 
         [Header("宝石ドロップアニメーション設定")]
         [SerializeField] private float _height = 1f;
@@ -23,59 +36,96 @@ namespace September.InGame.Jewelry
         [SerializeField] private AnimationCurve _horizontalEase;
 
         private IJewelryContainer _jewelryContainer;
+        private HitData _recentHitData;
+        private TickTimer _recentHitRetentionTimer;
 
         private Vector3 _prevGroundedPos;
 
-        public void Start()
+        public override void Spawned()
         {
+            if (!HasStateAuthority) return;
+
             _jewelryContainer = _jewelryContainerObj as IJewelryContainer;
             _playerRespawn.OnOutFieldEvent += OnOutField;
+            _playerHealth.OnHitTaken += OnHitTaken;
         }
 
-        private void Update()
+        public override void FixedUpdateNetwork()
         {
+            if (!HasStateAuthority) return;
+
             if (_playerMovement.IsGroundNet)
             {
                 _prevGroundedPos = transform.position;
+            }
+
+            if (_recentHitRetentionTimer.Expired(Runner))
+            {
+                _recentHitData = new HitData();
             }
         }
 
         private void OnOutField()
         {
-            int count = _jewelryContainer.GetJewelryCount(JewelryType.NormalGem) / 2;
-            var dropped = new IJewelry[count];
-            _jewelryContainer.DropJewelry(JewelryType.NormalGem, count, dropped);
-
-            DebugDrawUtility.DrawWireSphere(_prevGroundedPos, 1f, Color.green, 5f);
-
-            foreach (IJewelry drop in dropped)
+            foreach (JewelryDropSettingsContainer dropSettings in _dropSettings)
             {
-                if (drop is global::InGame.Jewelry.Jewelry jewelry)
+                HandleJewelryDrop(dropSettings.Settings);
+            }
+
+            JewelryDropLogger.OutputLog();
+
+            return;
+
+            void HandleJewelryDrop(JewelryDropSettings dropSettings)
+            {
+                int count = dropSettings.GetDropAmount(_recentHitData, _jewelryContainer, true);
+                var dropped = new IJewelry[count];
+                _jewelryContainer.DropJewelry(dropSettings.JewelryType, count, dropped);
+
+                DebugDrawUtility.DrawWireSphere(_prevGroundedPos, 1f, Color.green, 5f);
+
+                foreach (IJewelry drop in dropped)
                 {
-                    Vector3 pos = GetRandomPosition();
-                    DebugDrawUtility.DrawWireSphere(pos, 1f, Color.red, 5f);
-                    jewelry.JewelryControl.ThrowToNonPhysics(
-                        pos,
-                        (pos.y - transform.position.y) + _height + Random.Range(-.5f, .5f) * _randomHeight,
-                        _duration + Random.Range(-.5f, .5f) * _randomDuration,
-                        Random.value * _randomDelay,
-                        _verticalEase,
-                        _horizontalEase);
+                    if (drop is global::InGame.Jewelry.Jewelry jewelry)
+                    {
+                        if (_recentHitData.ExecutorRef.IsNone)
+                        {
+                            Vector3 pos = GetRandomPosition();
+                            DebugDrawUtility.DrawWireSphere(pos, 1f, Color.red, 5f);
+                            jewelry.JewelryControl.ThrowToNonPhysics(
+                                pos,
+                                (pos.y - transform.position.y) + _height + Random.Range(-.5f, .5f) * _randomHeight,
+                                _duration + Random.Range(-.5f, .5f) * _randomDuration,
+                                Random.value * _randomDelay,
+                                _verticalEase,
+                                _horizontalEase);
+                        }
+                        else
+                        {
+                            jewelry.PickupFrom(_recentHitData.ExecutorRef).Forget();
+                        }
+                    }
                 }
+            }
+
+            Vector3 GetRandomPosition()
+            {
+                var insideUnitCircle = Random.insideUnitCircle;
+                var randomOffset = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y) * 2f;
+                var target = _prevGroundedPos + randomOffset;
+                if (NavMesh.SamplePosition(target, out NavMeshHit hit, 10f, NavMesh.AllAreas))
+                {
+                    return hit.position;
+                }
+
+                return _prevGroundedPos;
             }
         }
 
-        private Vector3 GetRandomPosition()
+        private void OnHitTaken(HitData hitData)
         {
-            var insideUnitCircle = Random.insideUnitCircle;
-            var randomOffset = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y) * 2f;
-            var target = _prevGroundedPos + randomOffset;
-            if (NavMesh.SamplePosition(target, out NavMeshHit hit, 10f, NavMesh.AllAreas))
-            {
-                return hit.position;
-            }
-
-            return _prevGroundedPos;
+            _recentHitData = hitData;
+            _recentHitRetentionTimer = TickTimer.CreateFromSeconds(Runner, _hitRetentionTime);
         }
     }
 }
