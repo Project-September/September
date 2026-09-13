a175ecf255245cbc153d54f029fa0dfe94889640
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-09-14 00:27:37 +0900

    feat: 宝石スポーンエフェクト追加
    - 使いまわしやすいよう、Jewelryコンポーネントにメソッドとして実装
    - スポーンエフェクトの再生中は物理移動をしない
    - スポーンエフェクトの再生中は宝石を取得できないように
    
    赤い宝石のライトがズレている問題を修正

--- Assets/Scripts/InGame/Jewelry/Jewelry.cs
+++ Assets/Scripts/InGame/Jewelry/Jewelry.cs
@@ -11,9 +11,17 @@ namespace InGame.Jewelry
 {
     public class Jewelry : NetworkBehaviour, IJewelry
     {
-        [Header("この宝石のパラメータ群"), SerializeField] JewelryInfo _jewelryParams;
+        [Header("この宝石のパラメータ群")]
+        [SerializeField] JewelryInfo _jewelryParams;
         [SerializeField] JewelryControl _jewelryControl;
-        [SerializeField] ParticleSystemRenderer[] _renderers;
+
+        [Header("宝石エフェクト")]
+        [SerializeField] private ParticleSystem _jewelryEffect;
+        [SerializeField] private ParticleSystemRenderer[] _renderers;
+
+        [Header("出現エフェクト")]
+        [SerializeField] private ParticleSystem _spawnEffect;
+        [SerializeField] private float _spawnTimeOffset = 0.9f;
 
         public JewelryInfo JewelryParams => _jewelryParams;
         public JewelryControl JewelryControl => _jewelryControl;
@@ -80,6 +88,33 @@ namespace InGame.Jewelry
         {
             EffectSpawner effectSpawner = StaticServiceLocator.Instance.Get<EffectSpawner>();
             effectSpawner.RequestPlayOneShotEffect(_jewelryParams.PickupEffectType, transform.position + _jewelryParams.PickupEffectOffset, transform.rotation);
+        }
+
+        [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+        public void RPC_PlaySpawnEffect()
+        {
+            PlaySpawnEffect().Forget();
+        }
+
+        private async UniTask PlaySpawnEffect()
+        {
+            if (!_spawnEffect || !_jewelryEffect || !_jewelryControl)
+            {
+                Debug.LogWarning("SpawnEffectの再生に失敗しました", this);
+                return;
+            }
+
+            // スポーン前扱いなので非表示・動かない
+            _jewelryControl.Freeze();
+            _jewelryEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+            // スポーンエフェクトを再生
+            _spawnEffect.Play(true);
+            await UniTask.WaitForSeconds(_spawnTimeOffset, cancellationToken: destroyCancellationToken);
+
+            // フラッシュのタイミングで表示・動作開始
+            _jewelryControl.UnFreeze();
+            _jewelryEffect.Play(true);
         }
     }
 }
--- Assets/Scripts/InGame/Jewelry/JewelrySpawner.cs
+++ Assets/Scripts/InGame/Jewelry/JewelrySpawner.cs
@@ -229,7 +229,12 @@ namespace InGame.Jewelry
                 return null;
             }
 
-            return Runner.Spawn(prefab, position, Quaternion.identity);
+            NetworkObject obj = Runner.Spawn(prefab, position, Quaternion.identity);
+            if (obj.TryGetComponent(out Jewelry jewelry))
+            {
+                jewelry.RPC_PlaySpawnEffect();
+            }
+            return obj;
         }
 
         public override void Despawned(NetworkRunner runner, bool hasState)
