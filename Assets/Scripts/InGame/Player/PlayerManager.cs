6c728f05d86805261af4656d329af65f403c068b
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2026-09-14 10:36:13 +0900

    fix: allow stunned players to fall vertically

--- Assets/Scripts/InGame/Player/PlayerManager.cs
+++ Assets/Scripts/InGame/Player/PlayerManager.cs
@@ -383,6 +383,7 @@ namespace InGame.Player
         {
             _playerHealth.IsInvincible = false;
             IsStun = false;
+            _playerMovement.ResetHorizontalVelocity();
             RPC_SetPositionLock(false);
             _playerEffectController.StopStunEffect();
             _buildGenerator?.UpdateBuild(BuildRouteType.StunResistance);
@@ -394,6 +395,7 @@ namespace InGame.Player
             // ビルドの減衰分を乗算
             StunTickTimer = TickTimer.CreateFromSeconds(Runner, _stunTime * (_playerStatus ? _playerStatus.StunDurationMultiply : 1));
             IsStun = true;
+            _playerMovement.ResetHorizontalVelocity();
             RPC_SetPositionLock(true);
             _playerEffectController.PlayStunEffect();
         }
@@ -441,14 +443,13 @@ namespace InGame.Player
         [Rpc(RpcSources.All, RpcTargets.All)]
         public void RPC_SetPositionLock(NetworkBool isLocked)
         {
-            if (isLocked)
-            {
-                _rigidbody.linearVelocity = Vector3.zero;
-                _rigidbody.angularVelocity = Vector3.zero;
-            }
+            Vector3 velocity = _rigidbody.linearVelocity;
+            _rigidbody.linearVelocity = new Vector3(0f, velocity.y, 0f);
+            _rigidbody.angularVelocity = Vector3.zero;
 
             _rigidbody.constraints = isLocked ?
-                RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation :
+                RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ |
+                RigidbodyConstraints.FreezeRotation :
                 _defaultConstraints;
         }
 
