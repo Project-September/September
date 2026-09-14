10d55e5a5e488bdb90a792bd26f20ab1d8ed95b4
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2026-09-14 10:02:40 +0900

    fix:レビュー対応

--- Assets/Scripts/InGame/Exhibit/Tyranno/TyrannoInteractEffect.cs
+++ Assets/Scripts/InGame/Exhibit/Tyranno/TyrannoInteractEffect.cs
@@ -41,7 +41,7 @@ namespace InGame.Exhibit
                 return;
             }
 
-            if (playerInput.Buttons.IsSet(PlayerButtons.AirPlaneBack) && _interactTimer > 1f)
+            if (playerInput.Buttons.IsSet(PlayerButtons.Evasion) && _interactTimer > 1f)
             {
                 GetOff();
                 return;
