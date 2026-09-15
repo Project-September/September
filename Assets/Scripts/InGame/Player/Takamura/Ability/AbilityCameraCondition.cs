0d23c21ae0a4a9a212999a94f0edc85e7755f7a9
KNTy1018 <soramametaro1018@gmail.com>
2026-09-15 18:51:12 +0900

    fix:エクスカリバー時のバグ修正

--- Assets/Scripts/InGame/Player/Takamura/Ability/AbilityCameraCondition.cs
+++ Assets/Scripts/InGame/Player/Takamura/Ability/AbilityCameraCondition.cs
@@ -24,7 +24,7 @@ namespace InGame.Player.Ability.Condition
                 && typed.CurrentAbilityPhase == ScanAbilityPhase.Default;
 
             //Available状態でAttackボタンが押されたら条件を満たす
-            return _playerMovement.IsGround && !_playerManager.IsStun && !IsGameEnded() &&
+            return _playerMovement.IsGround && !_playerManager.IsStun && !_playerMovement.IsEvading && !IsGameEnded() &&
                    _playerManager.CurrentPlayerControlState == PlayerManager.PlayerControlState.Normal &&
                    context.AbilityRef.Phase == AbilityBase.AbilityPhase.Available &&
                    context.AbilityRef.CanStartAbilityOverride() &&
