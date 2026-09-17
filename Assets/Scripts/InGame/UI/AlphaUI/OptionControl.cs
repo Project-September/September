69f58646dd2025d4affaa8dd032b77e97cc0f817
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-09-17 17:40:13 +0900

    fix: 間違えて上げてしまった変更を戻す

--- Assets/Scripts/InGame/UI/AlphaUI/OptionControl.cs
+++ Assets/Scripts/InGame/UI/AlphaUI/OptionControl.cs
@@ -20,14 +20,6 @@ namespace InGame.UI
             Initialize();
         }
 
-        private void Update()
-        {
-            var settings = UserSettings.Get();
-            _bgmVolumeSlider.SetValueWithoutNotify(settings.BGMVolume);
-            _seVolumeSlider.SetValueWithoutNotify(settings.SEVolume);
-            _voiceVolumeSlider.SetValueWithoutNotify(settings.VoiceVolume);
-        }
-
         private void Initialize()
         {
             var settings = UserSettings.Get();
